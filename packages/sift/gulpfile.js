b10b566e2a5a4135c482801147b99ec92dcd811a
John J Czaplewski <jczaplew@gmail.com>
2015-05-06 16:47:29 -0500

    Reorganizing

--- packages/sift/gulpfile.js
+++ packages/sift/gulpfile.js
@@ -4,20 +4,26 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     watch = require('gulp-watch');
 
-gulp.task('default', function() {
-  gulp.src(['js/jquery-2.1.3.min.js', 'js/fastclick.min.js', 'js/leaflet.js', 'js/leaflet-hash.js', 'js/mustache.min.js', 'js/typeahead.bundle.min.js', 'js/topojson.min.js'])
+gulp.task('uglifyJS', function() {
+  gulp.src(['js/lib/jquery-2.1.3.min.js', 'js/lib/fastclick.min.js', 'js/lib/leaflet.js', 'js/lib/leaflet-hash.js', 'js/lib/mustache.min.js', 'js/lib/typeahead.bundle.min.js', 'js/lib/topojson.min.js', 'js/lib/underscore.min.js'])
     .pipe(concat('libs.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./js/'));
+});
 
-  gulp.src(['css/normalize.css', 'css/skeleton.css', 'css/leaflet.css', 'css/common.css', 'css/typeahead.css'])
+gulp.task('minifyCSS', function() {
+  gulp.src(['css/lib/normalize.css', 'css/lib/skeleton.css', 'css/lib/leaflet.css', 'css/common.css', 'css/lib/typeahead.css'])
     .pipe(concat('styles.min.css'))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./css/'));
+})
+
+gulp.task('default', 'uglifyJS', 'minifyCSS'], function() {
+
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch('css/*', ['default']);
-  gulp.watch('js/*', ['default']);
+  gulp.watch('css/*', ['minifyCSS']);
+  gulp.watch('js/*', ['uglifyJS']);
 });
