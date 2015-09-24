99eb6e9b22ad9359fa8606a976769f5a34010b7b
John J Czaplewski <jczaplew@gmail.com>
2015-09-24 16:33:56 -0500

    Redo landing page

--- packages/sift/src/js/components/Main.js
+++ packages/sift/src/js/components/Main.js
@@ -5,13 +5,19 @@ import RandomColumn from './RandomColumn';
 class Main extends React.Component {
 
   render() {
+    var options = [78, 137, 144, 434, 468, 474, 486, 488, 521, 1472, 1643];
+    var randomColID = options[Math.floor(Math.random() * (10 - 1) + 1) + 1]
+
     return (
       <div className='main'>
-        <div className='main-search'>
+        <div className='main-search' style={{backgroundImage: 'url(dist/img/' + randomColID + '.jpg);'}}>
           <div className='main-title'>
-            <h3>SIFT</h3>
+            <div className='main-title-container'>
+              <h3>SIFT</h3>
+              <p className='winnow'>& winnow</p>
+              <p className='macrostrat'>MACROSTRAT</p>
+            </div>
           </div>
-
         </div>
         <Stats/>
         <RandomColumn/>
