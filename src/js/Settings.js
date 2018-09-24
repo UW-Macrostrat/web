d1a2e05a64b16371b7059cbfec975928ae2234b4
John J Czaplewski <john@czaplewski.org>
2018-09-24 16:23:38 -0500

    Add settings

---
+++ src/js/Settings.js
@@ -0,0 +1,10 @@
+export const SETTINGS = {
+  baseMapURL: 'dist/style.json',
+  satelliteMapURL: 'mapbox://styles/jczaplewski/cj3bpe4xk00002rqndidf9dw4?optimize=true',
+  burwellTileDomain: 'https://devtiles.macrostrat.org',
+  //burwellTileDomain: 'http://localhost:5555',
+  apiDomain: 'https://dev.macrostrat.org',
+//  apiDomain: 'http://localhost:5000',
+  gddDomain: 'https://geodeepdive.org',
+  pbdbDomain: 'https://paleobiodb.org'
+}
