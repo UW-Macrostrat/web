5811edffe009d14e9d87b1bc651e3ac14494db49
Daven Quinn <dev@davenquinn.com>
2018-10-27 04:16:21 -0500

    Added filters

--- src/js/components/Filters.js
+++ src/js/components/Filters.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
 import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -8,7 +6,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline'
-import CloseIcon from '@material-ui/icons/Close'
+import {CloseableCard} from './CloseableCard'
 
 class Filters extends Component {
   constructor(props) {
@@ -18,19 +16,10 @@ class Filters extends Component {
     const { filtersOpen, toggleFilters } = this.props
 
     return (
-      <Dialog
-        open={filtersOpen}
-        onBackdropClick={toggleFilters}
-        aria-labelledby="filters">
-        <DialogTitle id="filter-title">
-          Filters
-
-          <IconButton color="default" aria-label="Menu" onClick={toggleFilters}>
-            <CloseIcon/>
-          </IconButton>
-
-        </DialogTitle>
-        <div>
+      <CloseableCard
+        isOpen={filtersOpen}
+        onClose={toggleFilters}
+        title="Filters">
           <List>
             <ListItem className={this.props.filters.length > 0 ? 'hidden' : ''}>
               <ListItemText primary='No filters applied'/>
@@ -46,8 +35,7 @@ class Filters extends Component {
               </ListItem>
             })}
           </List>
-        </div>
-      </Dialog>
+      </CloseableCard>
     )
   }
 }
