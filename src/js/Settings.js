f995233f62b10ac55979e5ba6c3acdb9bcd9ed23
John J Czaplewski <john@czaplewski.org>
2018-12-13 13:17:05 -0600

    update domains

--- src/js/Settings.js
+++ src/js/Settings.js
@@ -1,7 +1,7 @@
 export const SETTINGS = {
   baseMapURL: 'mapbox://styles/jczaplewski/cje04mr9l3mo82spihpralr4i?optimize=true',
   satelliteMapURL: 'mapbox://styles/jczaplewski/cj3bpe4xk00002rqndidf9dw4?optimize=true',
-  burwellTileDomain: 'https://tiles.macrostrat.org',
+  burwellTileDomain: (window.location.hostname === 'localhost' ? 'https://tiles.macrostrat.org' : ((window.location.hostname === 'dev.macrostrat.org') ? 'https://devtiles.macrostrat.org' : 'https://tiles.macrostrat.org')),
   apiDomain: (window.location.hostname === 'localhost') ? 'https://dev.macrostrat.org' : `https://${window.location.hostname}`,
   gddDomain: 'https://geodeepdive.org',
   pbdbDomain: 'https://paleobiodb.org',
