0503e801961f23699be142931ceb2521decf6580
Daven Quinn <dev@davenquinn.com>
2020-04-27 04:32:05 -0500

    A basic toggle button

--- src/js/map-page/index.tsx
+++ src/js/map-page/index.tsx
@@ -1,23 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 // Import other components
 import MapContainer from './map-view'
+import h from '@macrostrat/hyper'
 import SearchbarContainer from '../containers/SearchbarContainer'
 import MenuContainer from '../containers/MenuContainer'
 import InfoDrawerContainer from '../containers/InfoDrawerContainer'
 import FiltersContainer from '../containers/FiltersContainer'
 import AboutContainer from '../containers/AboutContainer'
 import ElevationChartContainer from '../containers/ElevationChartContainer'
+import {ButtonGroup, Button} from '@blueprintjs/core'
+
+enum MapBackend { MAPBOX, CESIUM }
+
+const MapView = (props: {backend: MapBackend}) =>{
+  switch (props.backend) {
+  case MapBackend.MAPBOX:
+    return h(MapContainer)
+  case MapBackend.CESIUM:
+    return h("div")
+  }
+}
+
+type TypeSelectorProps = {
+  backend: MapBackend,
+  setBackend(b: MapBackend): void
+}
+
+const MapTypeSelector = (props: TypeSelectorProps)=>{
+  const {backend, setBackend} = props
+  return h(ButtonGroup, [
+    h(Button, {
+      active: backend==MapBackend.MAPBOX,
+      onClick() { setBackend(MapBackend.MAPBOX)}
+    }, "2D"),
+    h(Button, {
+      active: backend==MapBackend.CESIUM,
+      onClick() { setBackend(MapBackend.CESIUM)}
+    }, "3D")
+  ])
+}
 
 const MapPage = ()=> {
+
+  const [backend, setBackend] = useState(MapBackend.MAPBOX)
+
   return (
     <div id="map-page">
-      <MapContainer/>
+      <MapView backend={backend} />
       <div className="ui">
         <div className="left-stack">
           <SearchbarContainer/>
           <MenuContainer/>
           <FiltersContainer/>
           <AboutContainer/>
+          <div className="spacer" />
+          <MapTypeSelector backend={backend} setBackend={setBackend} />
         </div>
         <InfoDrawerContainer/>
         <ElevationChartContainer/>
