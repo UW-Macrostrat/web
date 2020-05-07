0d4a6a6f9b7340778f3455fb0c3583edc412d37e
Daven Quinn <dev@davenquinn.com>
2020-05-07 03:58:19 -0500

    Move to 'resium' wrapper library

--- src/js/map-page/cesium-view/index.ts
+++ src/js/map-page/cesium-view/index.ts
@@ -1,110 +1,100 @@
-import { useEffect } from 'react'
+//https://resium.darwineducation.com/examples/?path=/story/fog--basic
+
+import { useRef, useEffect, ComponentProps} from 'react'
 import "cesiumSource/Widgets/widgets.css"
 import * as Cesium from "cesiumSource/Cesium"
 import {hyperStyled} from '@macrostrat/hyper'
 import styles from "./main.styl"
 const h = hyperStyled(styles)
 import {useSelector, useDispatch } from 'react-redux'
-import NavigationMixin from "@znemz/cesium-navigation"
-import "@znemz/cesium-navigation/dist/index.css"
+import {GlobeViewer} from './viewer'
+
+import {
+  ImageryLayer,
+  ScreenSpaceEventHandler,
+  ScreenSpaceEvent,
+  Entity,
+  useCesium
+} from "resium"
 import {
   queryMap
 } from '../../actions'
 
-const CesiumView = (props)=>{
-  const mapOpts = useSelector(s => s.update)
-  const {mapXYZ} = mapOpts
+const terrainProvider = Cesium.createWorldTerrain();
 
+const MapClickHandler = ()=>{
   const dispatchAction = useDispatch()
+  const {viewer} = useCesium()
 
-  useEffect(()=>{
-    var geology = new Cesium.WebMapTileServiceImageryProvider({
-  		url : 'https://macrostrat.org/api/v2/maps/burwell/emphasized/{TileMatrix}/{TileCol}/{TileRow}/tile.png',
-  		style : 'default',
-  		format : 'image/png',
-  		maximumLevel : 19,
-      layer: "",
-      tileMatrixSetID: "",
-  		credit : new Cesium.Credit('UW-Madison, Macrostrat Lab'),
-  	})
+  const clickPoint = (movement)=>{
+    var cartesian = viewer.camera.pickEllipsoid(movement.position, viewer.scene.globe.ellipsoid);
 
+    var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+    const longitude = Cesium.Math.toDegrees(cartographic.longitude);
+    const latitude = Cesium.Math.toDegrees(cartographic.latitude);
+    //console.log(longitude, latitude);
+    //addPoint(longitude, latitude)
+    dispatchAction(queryMap(longitude, latitude, 7, null))
+  }
 
-    var opts = {
-      terrainProvider: Cesium.createWorldTerrain(),
-      imageryProvider : Cesium.createWorldImagery({
-        style : Cesium.IonWorldImageryStyle.AERIAL
-      }),
-      baseLayerPicker : false,
-      fullscreenButton: false,
-      homeButton: false,
-      infoBox: false,
-      navigationInstructionsInitiallyVisible: false,
-      navigationHelpButton: false,
-      scene3DOnly: true,
-      vrButton: false,
-      geocoder: false,
-      //skyAtmosphere: true,
-      animation: false,
-      timeline: false,
-      // Makes cesium not render high fps all the time
-      requestRenderMode : true,
-      // Use full scene buffer (respecting pixel ratio) if this is false
-      useBrowserRecommendedResolution: false
-    }
+  return h(ScreenSpaceEventHandler, [
+    h(ScreenSpaceEvent, {
+      action: clickPoint,
+      type: Cesium.ScreenSpaceEventType.LEFT_CLICK
+    })
+  ])
+}
 
+type GeoLayerProps = Omit<ComponentProps<typeof ImageryLayer>,"imageryProvider">
 
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzODk2OGM4ZS1mMzlkLTRlNjAtYWQxZS1mODU3YWJjMWFhNzQiLCJpZCI6MjYwODYsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1ODcxOTU1MTh9._ILy51LI2aF7Nxvas9RQDkhqOP4Tp92uTYAtvewVvNE';
-    var viewer = new Cesium.Viewer('cesiumContainer', opts)
-    //viewer.resolutionScale = 2
-    //viewer.scene.globe.enableLighting = true
-    //viewer.canvas.style.imageRendering = false
+const GeologyLayer = (props: GeoLayerProps)=>{
+  let geology = useRef(new Cesium.WebMapTileServiceImageryProvider({
+    url : 'https://macrostrat.org/api/v2/maps/burwell/emphasized/{TileMatrix}/{TileCol}/{TileRow}/tile.png',
+    style : 'default',
+    format : 'image/png',
+    maximumLevel : 19,
+    layer: "",
+    tileMatrixSetID: "",
+    credit : new Cesium.Credit('UW-Madison, Macrostrat Lab'),
+  }))
 
-    viewer.extend(NavigationMixin, {})
+  return h(ImageryLayer, {imageryProvider: geology.current, ...props})
+}
 
-    var geoLayer = viewer.imageryLayers.addImageryProvider(geology);
-    geoLayer.alpha = 0.5;
+const SelectedPoint = (props)=>{
+  const mapOpts = useSelector(s => s.update)
+  const {
+    infoMarkerLng,
+    infoMarkerLat,
+  } = mapOpts
+  // TODO: fix weird null signifier
+  if (infoMarkerLng == -999 || infoMarkerLat == -999) return null
 
-    const clon = parseFloat(mapXYZ.x)
-    const clat = parseFloat(mapXYZ.y)
-    var extent = Cesium.Cartesian3.fromDegrees(clon, clat, 800000)
-    viewer.camera.setView({
-        destination : extent,
-        orientation: {
-            heading : Cesium.Math.toRadians(0), // east, default value is 0.0 (north)
-            pitch : Cesium.Math.toRadians(-90),    // default value (looking down)
-            roll : 0.0                             // default value
-        }
-    });
+  let position = Cesium.Cartesian3.fromDegrees(
+    // TODO: Numbers should be guaranteed in typescript
+    parseFloat(infoMarkerLng),
+    parseFloat(infoMarkerLat)
+  )
+  let pointGraphics  = {
+    color : Cesium.Color.DODGERBLUE,
+    outlineColor : Cesium.Color.WHITE,
+    outlineWidth : 2,
+    pixelSize: 10
+  }
 
-    var clickedPoint = viewer.scene.primitives.add(new Cesium.PointPrimitiveCollection());
+  return h(Entity, {position, point: pointGraphics})
+}
 
-    const addPoint = (x: number, y: number)=>{
-      clickedPoint.removeAll()
-      clickedPoint.add({
-          position : new Cesium.Cartesian3.fromDegrees(x, y),
-          color : Cesium.Color.DODGERBLUE,
-          outlineColor : Cesium.Color.WHITE,
-          outlineWidth : 2
-      });
-    }
-
-    var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
-  	handler.setInputAction(function(movement) {
-  		var cartesian = viewer.camera.pickEllipsoid(movement.position, viewer.scene.globe.ellipsoid);
-  		if (cartesian) {
-  			var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-  			const longitude = Cesium.Math.toDegrees(cartographic.longitude);
-  			const latitude = Cesium.Math.toDegrees(cartographic.latitude);
-        console.log(longitude, latitude);
-        dispatchAction(queryMap(longitude, latitude, 7, null))
-        addPoint(longitude, latitude)
-  		}
-  	}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
-
-
-
-  })
-  return h("div.cesium-container#cesiumContainer")
+const CesiumView = (props)=>{
+  return h(GlobeViewer, {
+    terrainProvider,
+    highResolution: true
+  }, [
+    h(GeologyLayer, {alpha: 0.5}),
+    h(MapClickHandler),
+    h(SelectedPoint)
+    //h(Entity, {position, point: pointGraphics})
+  ])
 }
 
 export default CesiumView;
---
+++ src/js/map-page/cesium-view/viewer.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef, ComponentProps } from "react";
+import h from '@macrostrat/hyper'
+import { Viewer, CesiumComponentRef } from "resium";
+import NavigationMixin from "@znemz/cesium-navigation"
+import "@znemz/cesium-navigation/dist/index.css"
+
+type GlobeViewerProps = ComponentProps<typeof Viewer> & {
+  highResolution: boolean
+}
+
+const GlobeViewer = (props: GlobeViewerProps) => {
+  const ref = useRef<CesiumComponentRef<Cesium.Viewer>>(null);
+  const {highResolution, ...rest} = props
+
+  useEffect(() => {
+    const {cesiumElement} = ref.current ?? {}
+    if (cesiumElement == null) return
+    let ratio = 1
+    if (highResolution) {
+      ratio = Math.min(window.devicePixelRatio ?? 1, 2)
+    }
+    ref.current.cesiumElement.resolutionScale = ratio
+  }, [highResolution]);
+
+  useEffect(() => {
+    const {cesiumElement} = ref.current ?? {}
+    if (cesiumElement == null) return
+    ref.current.cesiumElement.extend(NavigationMixin, {})
+  }, []);
+
+  return h(Viewer, {
+    ref,
+    full: true,
+    baseLayerPicker : false,
+    fullscreenButton: false,
+    homeButton: false,
+    infoBox: false,
+    navigationInstructionsInitiallyVisible: false,
+    navigationHelpButton: false,
+    scene3DOnly: true,
+    vrButton: false,
+    geocoder: false,
+    //skyAtmosphere: true,
+    animation: false,
+    timeline: false,
+    ...rest
+  })
+};
+
+export {GlobeViewer}
