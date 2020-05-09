d3ab0142ccc30449e738136c7251fbf42f50062b
Daven Quinn <dev@davenquinn.com>
2020-05-09 13:47:48 -0500

    Simplify menu view

--- src/js/components/Menu.tsx
+++ src/js/components/Menu.tsx
@@ -7,6 +7,8 @@ import BedrockIcon from './icons/BedrockIcon'
 import {Button, ButtonGroup, Alignment, IButtonProps} from '@blueprintjs/core'
 import {CloseableCard} from './CloseableCard'
 import {useSelector, useDispatch} from 'react-redux'
+import {MenuPanel} from '../reducers/menu'
+import AboutText from './About'
 
 type ListButtonProps = IButtonProps & {icon: React.ComponentType | Pick<IButtonProps,"icon">}
 const ListButton = (props: ListButtonProps)=>{
@@ -18,6 +20,14 @@ const ListButton = (props: ListButtonProps)=>{
 }
 
 const MinimalButton = (props)=>h(Button, {...props, minimal: true})
+
+const TabButton = (props: IButtonProps & {tab: MenuPanel})=>{
+  const {tab, ...rest} = props
+  const dispatch = useDispatch()
+  const onClick = ()=>dispatch({type: 'set-panel', panel: tab})
+  const active = useSelector(state => state.menu.activePanel == tab)
+  return h(MinimalButton, {active, onClick, ...rest})
+}
 
 const LayerButton = (props: ListButtonProps & {layer: string} )=>{
   const {layer, ...rest} = props
@@ -41,14 +51,60 @@ const MenuGroup = (props)=> h(ButtonGroup, {
   ...props
 })
 
-const Menu = (props)=>{
+const LayerList = (props)=>{
+  const dispatch = useDispatch()
 
   const toggleElevationChart = ()=>{
-    props.toggleMenu()
-    props.toggleElevationChart()
+    dispatch({type: "TOGGLE_MENU"})
+    dispatch({type: "TOGGLE_ELEVATION_CHART"})
   }
 
-  const {menuOpen, toggleMenu, toggleAbout} = props
+  return h("div.menu-content", [
+    h(MenuGroup, [
+      h(LayerButton, {
+        layer: "Bedrock",
+        icon: BedrockIcon
+      }),
+      h(LayerButton, {
+        layer: "Lines",
+        icon: LineIcon
+      }),
+      h(LayerButton, {
+        layer: "Columns",
+        icon: ColumnIcon
+      }),
+      h(LayerButton, {
+        layer: "Fossils",
+        icon: FossilIcon
+      }),
+      h(LayerButton, {
+        layer: "Satellite",
+        icon: 'satellite'
+      }),
+    ]),
+    h(MenuGroup, [
+      h(ListButton, {disabled: true, icon: 'map-marker'}, "Your location"),
+      h(ListButton, {onClick: toggleElevationChart, icon: ElevationIcon}, "Elevation profile")
+    ])
+  ])
+}
+
+const PanelContent = (props: {activePanel: MenuPanel})=>{
+  const activePanel = useSelector(state => state.menu.activePanel)
+  switch (activePanel) {
+  case MenuPanel.LAYERS:
+    return h(LayerList)
+  case MenuPanel.SETTINGS:
+    return h("div")
+  case MenuPanel.ABOUT:
+    return h(AboutText)
+  }
+  return null
+}
+
+const Menu = (props)=>{
+
+  const {menuOpen, toggleMenu} = props
 
   let exitTransition = {exit: 300}
 
@@ -59,40 +115,13 @@ const Menu = (props)=>{
     transitionDuration: exitTransition
   }, [
     h(CloseableCard.Header, [
-      h(ButtonGroup, [
-        h(MinimalButton, {icon: "layers", text: "Layers"}),
-        h(MinimalButton, {icon: "settings", text: "Settings"}),
-        h(MinimalButton, {icon: "info-sign", text: "About" , onClick: toggleAbout})
+      h("div.buttons", [
+        h(TabButton, {icon: "layers", text: "Layers", tab: MenuPanel.LAYERS}),
+        h(TabButton, {icon: "settings", text: "Settings", tab: MenuPanel.SETTINGS}),
+        h(TabButton, {icon: "info-sign", text: "About" , tab: MenuPanel.ABOUT})
       ])
     ]),
-    h("div.menu-content", [
-      h(MenuGroup, [
-        h(LayerButton, {
-          layer: "Bedrock",
-          icon: BedrockIcon
-        }),
-        h(LayerButton, {
-          layer: "Lines",
-          icon: LineIcon
-        }),
-        h(LayerButton, {
-          layer: "Columns",
-          icon: ColumnIcon
-        }),
-        h(LayerButton, {
-          layer: "Fossils",
-          icon: FossilIcon
-        }),
-        h(LayerButton, {
-          layer: "Satellite",
-          icon: 'satellite'
-        }),
-      ]),
-      h(MenuGroup, [
-        h(ListButton, {disabled: true, icon: 'map-marker'}, "Your location"),
-        h(ListButton, {onClick: toggleElevationChart, icon: ElevationIcon}, "Elevation profile")
-      ])
-    ])
+    h(PanelContent)
   ])
 }
 
