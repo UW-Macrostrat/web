363338b190e29f6396895d8c799186575c3f5ad7
Casey Idzikowski <idzikoj1234@gmail.com>
2021-04-22 16:55:11 -0400

    shared verticies will snap on drag. Fixed in same feature bug and triple junction bug

--- packages/column-footprint-editor/src/components/map/mapgl.tsx
+++ packages/column-footprint-editor/src/components/map/mapgl.tsx
@@ -6,13 +6,24 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import mapboxgl from "mapbox-gl";
 
-import { TopoJSONToLineString } from "./utils";
+import { TopoJSONToLineString, coordinatesAreEqual } from "./utils";
 import "./map.css";
+
+/**
+ *
+ * Edge cases to fix:
+ *
+ */
 
 // Topojson
 // topojson.feautre(topology, object) returns geojson
 // topojson.mesh(topology, object, filter) returns merged arcs as linestrings
 // topojson.neighbors(objects) list of adjacent objects
+
+interface tomove {
+  id: string;
+  index: number;
+}
 
 const url =
   "https://macrostrat.org/api/v2/columns?project_id=1&format=topojson_bare";
@@ -36,8 +47,6 @@ export function Map() {
     }
   }, [topo]);
 
-  topo ? console.log(topo) : console.log("...loading"); //topo.objects.output
-
   useEffect(() => {
     if (!topo) return;
     var map = new mapboxgl.Map({
@@ -47,20 +56,95 @@ export function Map() {
       zoom: zoom, // starting zoom
     });
 
+    let toMoveCoordPath;
+    let toMoveFeature;
+    let movedCoordPath;
+    let sameFeature;
+
+    console.log(toMoveCoordPath);
+
     var nav = new mapboxgl.NavigationControl();
 
     map.addControl(nav);
 
+    const MultipleVerticiesClick = MapboxDraw.modes.simple_select;
+
+    // need to just pass off it there aren't other verticies at point
+    MultipleVerticiesClick.clickOnVertex = function(state, e) {
+      console.log(e);
+      console.log(this._ctx);
+
+      // this block gets features other than the clicked one at point
+      var point = map.project(e.lngLat);
+      const idsAtPoint = this._ctx.api.getFeatureIdsAt(point);
+      let features = idsAtPoint.map((id) => this.getFeature(id));
+
+      const currentId = e.featureTarget.properties.parent;
+      const currentFeature = this.getFeature(currentId);
+      const targetCoords = e.featureTarget.geometry.coordinates;
+
+      features = features.filter((f) => f != null && f.id != currentId); // this will return the other vertix
+
+      if (features.length > 0) {
+        movedCoordPath = e.featureTarget.properties.coord_path;
+
+        const coords = [...targetCoords];
+
+        toMoveFeature = features;
+
+        let match = [];
+        features.map((f) => {
+          if (f) {
+            let coord_path = f.coordinates.map((coord, index) => {
+              if (coordinatesAreEqual({ coord1: coord, coord2: coords })) {
+                match.push(index);
+              }
+            });
+          }
+        });
+        toMoveCoordPath = match; // will have same index as toMoveFeature
+      } else {
+        let truthy = [];
+        let coordPaths = [];
+        currentFeature.coordinates.map((coord, index) => {
+          if (coordinatesAreEqual({ coord1: coord, coord2: targetCoords })) {
+            truthy.push(1);
+            coordPaths.push(index);
+          }
+        });
+        if (truthy.length > 0) {
+          // need to set to moveId to an array of the pathcoords that are the same
+          console.log("More than one!!");
+          console.log(coordPaths);
+          toMoveCoordPath = coordPaths;
+          toMoveFeature = currentFeature;
+          movedCoordPath = e.featureTarget.properties.coord_path;
+          sameFeature = true;
+        }
+      }
+
+      // this is what the normal simple_select does, we want to keep that the same
+      this.changeMode("direct_select", {
+        featureId: e.featureTarget.properties.parent,
+        coordPath: e.featureTarget.properties.coord_path,
+        startPos: e.lngLat,
+      });
+    };
+
     /// draw.create, draw.delete, draw.update, draw.selectionchange
     /// draw.modechange, draw.actionable, draw.combine, draw.uncombine
-    var Draw = new MapboxDraw();
+    var Draw = new MapboxDraw({
+      defaultMode: "mult_vert",
+      modes: Object.assign(
+        {
+          mult_vert: MultipleVerticiesClick,
+        },
+        MapboxDraw.modes
+      ),
+    });
     map.addControl(Draw, "top-left");
 
     var featureIds = Draw.add(topo);
-
-    map.on("draw.create", function(e) {
-      console.log(Draw.getAll());
-    });
 
     map.on("move", () => {
       setLng(map.getCenter().lng.toFixed(4));
@@ -68,10 +152,42 @@ export function Map() {
       setZoom(map.getZoom().toFixed(2));
     });
 
+    map.on("draw.trash", function(e) {
+      console.log("trash");
+      console.log(e);
+    });
+
     // maybe a custom editing that checks if there is another vertix at same point, and also select
     // that one.
+    // use the splice to replace coords
+    // This needs to account for deleteing nodes. That falls under change_coordinates
     map.on("draw.update", function(e) {
-      console.log(e);
+      console.log(toMoveCoordPath);
+      console.log(toMoveFeature);
+
+      let newCoord = e.features[0].geometry.coordinates[movedCoordPath];
+
+      if (sameFeature) {
+        //logic for if multiple verticies, same point, same feature
+        let coordsToChange = [...toMoveFeature.coordinates];
+        toMoveCoordPath.map((coordPath) => {
+          coordsToChange.splice(coordPath, 1, newCoord);
+        });
+        toMoveFeature.setCoordinates(coordsToChange);
+      } else {
+        // different features, need to adjust for triple junction
+        toMoveFeature.map((feature, index) => {
+          let coordsToChange = [...feature.coordinates];
+          coordsToChange.splice(toMoveCoordPath[index], 1, newCoord);
+          feature.setCoordinates(coordsToChange);
+        });
+      }
+
+      // reset the state to undefined
+      toMoveCoordPath = undefined;
+      toMoveFeature = undefined;
+      movedCoordPath = undefined;
+      sameFeature = undefined;
     });
     return () => map.remove();
   }, [topo]);
--- packages/column-footprint-editor/src/components/map/utils.ts
+++ packages/column-footprint-editor/src/components/map/utils.ts
@@ -25,4 +25,25 @@ function TopoJSONToLineString(json) {
   return featureCollection;
 }
 
-export { TopoJSONToLineString };
+/**
+ * Function tells whether too coordinates are the same or not
+ * @param props
+ * coord1:  [lng, lat]
+ * coord2: [lng,lat]
+ *
+ *
+ * @returns boolean
+ */
+function coordinatesAreEqual(props) {
+  const { coord1, coord2 } = props;
+  if (
+    coord1[0].toFixed(2) == coord2[0].toFixed(2) &&
+    coord1[1].toFixed(2) == coord2[1].toFixed(2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export { TopoJSONToLineString, coordinatesAreEqual };
