11bd7d73549e218d044053f934443a0a783c46f2
Daven Quinn <dev@davenquinn.com>
2021-05-17 02:12:05 -0500

    Make things load more properly

--- src/js/map-page/cesium-view.ts
+++ src/js/map-page/cesium-view.ts
@@ -4,28 +4,39 @@ import { useDispatch, useSelector } from "react-redux";
 import { queryMap, mapMoved } from "../actions";
 import {
   MapChangeTracker,
-  MapClickHandler,
+  MapClickHandler
 } from "@macrostrat/cesium-viewer/position";
+import {
+  HillshadeLayer,
+  GeologyLayer,
+  SatelliteLayer,
+  terrainProvider
+} from "@macrostrat/cesium-viewer/layers";
 
 function MacrostratCesiumView(props) {
   const dispatch = useDispatch();
   const terrainExaggeration =
-    useSelector((state) => state.globe.verticalExaggeration) ?? 1.0;
-  const displayQuality = useSelector((state) => state.globe.displayQuality);
+    useSelector(state => state.globe.verticalExaggeration) ?? 1.0;
+  const displayQuality = useSelector(state => state.globe.displayQuality);
 
-  const showInspector = useSelector((state) => state.globe.showInspector);
+  const showInspector = useSelector(state => state.globe.showInspector);
 
-  return h(CesiumView, {
-    onViewChange(cpos) {
-      dispatch(mapMoved(cpos));
+  return h(
+    CesiumView,
+    {
+      onViewChange(cpos) {
+        dispatch(mapMoved(cpos));
+      },
+      onClick({ latitude, longitude, zoom }) {
+        dispatch(queryMap(longitude, latitude, zoom, null));
+      },
+      terrainExaggeration,
+      displayQuality,
+      showInspector,
+      terrainProvider
     },
-    onClick({ latitude, longitude, zoom }) {
-      dispatch(queryMap(longitude, latitude, zoom, null));
-    },
-    terrainExaggeration,
-    displayQuality,
-    showInspector,
-  });
+    [h(HillshadeLayer), h(SatelliteLayer), h(GeologyLayer, { alpha: 0.5 })]
+  );
 }
 
 export default MacrostratCesiumView;
--- src/js/reducers/index.ts
+++ src/js/reducers/index.ts
@@ -1,13 +1,41 @@
 import { combineReducers } from "redux";
+import reduceReducers from "reduce-reducers";
 import { menuReducer } from "./menu";
-import { globeReducer } from "@macrostrat/cesium-viewer/actions";
+import { reducer as globeReducer } from "@macrostrat/cesium-viewer/actions";
+import { nadirCameraPosition } from "@macrostrat/cesium-viewer/position";
+import { GOT_INITIAL_MAP_STATE } from "../actions";
 import update from "./legacy";
 
 const reducers = combineReducers({
   // list reducers here
   menu: menuReducer,
   globe: globeReducer,
-  update,
+  update
 });
 
-export default reducers;
+function overallReducer(state, action) {
+  if (action.type === GOT_INITIAL_MAP_STATE) {
+    // You can access both app and inventory states here
+    const { x, y, z } = action.data;
+    const destination = nadirCameraPosition(
+      parseFloat(x),
+      parseFloat(y),
+      parseFloat(z)
+    );
+    console.log("Set globe position", destination);
+    const newState = {
+      ...state,
+      globe: {
+        ...state.globe,
+        flyToProps: { destination, duration: 0, once: true }
+      }
+    };
+    console.log(newState);
+    return newState;
+  }
+  return state;
+}
+
+const newReducer = reduceReducers(reducers, overallReducer);
+
+export default newReducer;
