3daa65e1e7bb0d865d99e5c56e5cf88385804036
Daven Quinn <dev@davenquinn.com>
2021-05-19 16:46:33 -0500

    Added cesium viewer updates

--- src/js/map-page/cesium-view.ts
+++ src/js/map-page/cesium-view.ts
@@ -1,4 +1,4 @@
-import CesiumView from "@macrostrat/cesium-viewer";
+import CesiumView, { DisplayQuality } from "@macrostrat/cesium-viewer";
 import h from "@macrostrat/hyper";
 import { useDispatch, useSelector } from "react-redux";
 import { queryMap, mapMoved } from "../actions";
@@ -8,16 +8,52 @@ import {
 } from "@macrostrat/cesium-viewer/position";
 import {
   HillshadeLayer,
-  GeologyLayer,
+  //GeologyLayer,
   SatelliteLayer,
   terrainProvider
 } from "@macrostrat/cesium-viewer/layers";
+import { ImageryLayer } from "resium";
+import { useMemo } from "react";
+import MVTImageryProvider from "mvt-imagery-provider";
+import { mapStyle } from "./vector-style";
+
+const GeologyLayer = ({ visibleMaps = null, ...rest }) => {
+  const provider = useMemo(() => {
+    let prov = new MVTImageryProvider({
+      style: mapStyle,
+      maximumZoom: 13,
+      tileSize: 512
+    });
+    // let filter: any = ["boolean", true];
+    // if (visibleMaps != null) {
+    //   filter = [
+    //     "match",
+    //     ["get", "map_id"],
+    //     Array.from(visibleMaps),
+    //     true,
+    //     false
+    //   ];
+    // }
+    // console.log(filter);
+    // prov.mapboxRenderer.setFilter("map-units", filter, false);
+    // const res = prov.mapboxRenderer.setFilter("unit-edge", filter, false);
+    // res();
+    return prov;
+  }, [visibleMaps]);
+
+  const hasGeology = useSelector(state => state.update.mapHasBedrock);
+
+  if (!hasGeology) return null;
+
+  return h(ImageryLayer, { imageryProvider: provider, ...rest });
+};
 
 function MacrostratCesiumView(props) {
   const dispatch = useDispatch();
   const terrainExaggeration =
     useSelector(state => state.globe.verticalExaggeration) ?? 1.0;
   const displayQuality = useSelector(state => state.globe.displayQuality);
+  const globe = useSelector(state => state.globe);
 
   const showInspector = useSelector(state => state.globe.showInspector);
 
@@ -25,15 +61,19 @@ function MacrostratCesiumView(props) {
     CesiumView,
     {
       onViewChange(cpos) {
-        dispatch(mapMoved(cpos));
+        console.log(cpos);
+        const { viewCenter } = cpos;
+        if (viewCenter == null) return;
+        dispatch(mapMoved(viewCenter));
       },
       onClick({ latitude, longitude, zoom }) {
         dispatch(queryMap(longitude, latitude, zoom, null));
       },
       terrainExaggeration,
-      displayQuality,
+      displayQuality: DisplayQuality.High,
       showInspector,
-      terrainProvider
+      terrainProvider,
+      flyTo: globe.flyToProps
     },
     [h(HillshadeLayer), h(SatelliteLayer), h(GeologyLayer, { alpha: 0.5 })]
   );
