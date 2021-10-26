f9fb94b1699572476c40e4318184852dc77500dc
Casey Idzikowski <idzikoj1234@gmail.com>
2021-10-26 20:05:37 +0200

    simplified environment variables

--- packages/column-footprint-editor/src/context/env.ts
+++ packages/column-footprint-editor/src/context/env.ts
@@ -1,11 +1,4 @@
-const prodOrDev = process.env.ENV;
 
-const dev_api_base = process.env.API_BASE_DEV;
-const prod_api_base = process.env.API_BASE_PROD;
-
-let base = dev_api_base;
-if (prodOrDev != "development") {
-  base = prod_api_base;
-}
+let base = process.env.API_BASE
 
 export { base };
