fa5807fe2191a26b9083dd38ccfd4e2847a04a9f
Casey Idzikowski <idzikoj1234@gmail.com>
2021-12-06 21:04:02 +0100

    Properties Modal is now draggable

--- packages/column-footprint-editor/src/components/blueprint/dialog.tsx
+++ packages/column-footprint-editor/src/components/blueprint/dialog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Overlay, Button, Card } from "@blueprintjs/core";
 import "./main.css";
 
@@ -12,6 +12,52 @@ function OverlayBox(props) {
     cardStyles = {},
   } = props;
 
+  const [state, setState] = useState({ top: 100, left: 20 });
+  const [offset, setOffset] = useState({ rel_x: 0, rel_y: 0 });
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    //setup event listeners
+    if (!dragging) return;
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    return () => {
+      document.removeEventListener("mouseup", onMouseUp);
+      document.removeEventListener("mousemove", onMouseMove);
+    };
+  }, [dragging]);
+
+  const onMouseDown = (e) => {
+    if (e.button !== 0) return;
+    setDragging(true);
+    setOffset(getOffest(e));
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const onMouseUp = (e) => {
+    setDragging(false);
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const getOffest = (e) => {
+    const rel_x = e.pageX - state.left;
+    const rel_y = e.pageY - state.top;
+    return { rel_x, rel_y };
+  };
+
+  const onMouseMove = (e) => {
+    if (dragging) {
+      const { rel_x, rel_y } = offset;
+      const left_ = e.pageX - rel_x;
+      const top_ = e.pageY - rel_y;
+      setState({ top: top_, left: left_ });
+    }
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
   const overlayProperties = {
     autoFocus: true,
     canEscapeKeyClose: true,
@@ -22,9 +68,11 @@ function OverlayBox(props) {
     useTallContent: false,
   };
 
+  const style = { top: `${state.top}px`, left: `${state.left}px` };
+
   return (
     <Overlay isOpen={open} {...overlayProperties}>
-      <div className={className}>
+      <div className={className} style={style} onMouseDown={onMouseDown}>
         <Card style={cardStyles}>
           {children}
           {closeButton ? (
