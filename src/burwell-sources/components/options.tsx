e20e9a2b0cf5a767dd8666b001fc322260ab6336
Casey Idzikowski <idzikoj1234@gmail.com>
2022-01-04 13:29:41 -0600

    Refactored options menu to be blueprint.js

--- src/burwell-sources/components/options.tsx
+++ src/burwell-sources/components/options.tsx
@@ -1,182 +1,71 @@
-import React from "react";
-import FlatButton from "material-ui/FlatButton";
-import Popover, { PopoverAnimationVertical } from "material-ui/Popover";
-import Menu from "material-ui/Menu";
-import MenuItem from "material-ui/MenuItem";
-import ChevronLeft from "material-ui/svg-icons/navigation/chevron-left";
-import NavigationCheck from "material-ui/svg-icons/navigation/check";
+import { Menu, MenuItem, Popover, Button, Icon } from "@blueprintjs/core";
 import {
   useBurwellActions,
   useBurwellState,
 } from "~/burwell-sources/app-state";
+import h from "@macrostrat/hyper";
 
-// const mapStateToProps = state => {
-//   return {
-//     optionsOpen: state.handleInteraction.optionsOpen,
-//     optionsAnchorElement: state.handleInteraction.optionsAnchorElement,
-//     selectedScale: state.handleInteraction.selectedScale,
-//     view: state.handleInteraction.view
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     openOptions: (event) => {
-//       dispatch(openOptions(event))
-//     },
-//     closeOptions: () => {
-//       dispatch(closeOptions())
-//     },
-//     selectScale: (scale) => {
-//       dispatch(selectScale(scale))
-//       dispatch(closeOptions())
-//     },
-//     changeView: (view) => {
-//       dispatch(changeView(view))
-//       dispatch(closeOptions())
-//       dispatch(toggleMenu(false))
-//     }
-//   }
-// }
-
-const Options = () => {
-  const { optionsOpen, optionsAnchorElement, selectedScale, view } =
-    useBurwellState((state) => state);
-  const runAction = useBurwellActions();
-
-  const openOptions = (el) => {
-    runAction({
-      type: "open-options",
-      optionsAnchorElement: el,
-      optionsOpen: true,
-    });
-  };
-
-  const closeOptions = () => {
-    runAction({ type: "close-options", optionsOpen: false });
-  };
-  const selectScale = (scale) => {
-    runAction({ type: "select-scale", selectedScale: scale });
-    closeOptions();
-  };
-  const changeView = (view) => {
-    runAction({ type: "change-view", view });
-    closeOptions();
-    runAction({ type: "toggle-menu", menuOpen: false });
-  };
-  
-  return (
-    <div>
-      <FlatButton
-        id="option-button"
-        onClick={() => {
-          openOptions(document.getElementById("option-button"));
-        }}
-        label="Options"
-      />
-      <Popover
-        open={optionsOpen}
-        anchorEl={optionsAnchorElement}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        targetOrigin={{ horizontal: "right", vertical: "top" }}
-        onRequestClose={() => {
-          closeOptions();
-        }}
-        animation={PopoverAnimationVertical}
-      >
-        <Menu>
-          <MenuItem
-            primaryText="View"
-            leftIcon={<ChevronLeft />}
-            menuItems={[
-              <MenuItem
-                primaryText="List"
-                value="list"
-                onClick={() => {
-                  changeView("list");
-                }}
-                rightIcon={view === "list" ? <NavigationCheck /> : null}
-              />,
-              <MenuItem
-                primaryText="Map"
-                value="map"
-                onClick={() => {
-                  changeView("map");
-                }}
-                rightIcon={view === "map" ? <NavigationCheck /> : null}
-              />,
-            ]}
-          />
-          <MenuItem
-            primaryText="Scale"
-            leftIcon={<ChevronLeft />}
-            value={selectedScale}
-            menuItems={[
-              <MenuItem
-                onClick={() => {
-                  selectScale("all");
-                }}
-                primaryText="All"
-                value="all"
-                rightIcon={selectedScale === "all" ? <NavigationCheck /> : null}
-              />,
-              <MenuItem
-                onClick={() => {
-                  selectScale("large");
-                }}
-                primaryText="Large"
-                value="large"
-                rightIcon={
-                  selectedScale === "large" ? <NavigationCheck /> : null
-                }
-              />,
-              <MenuItem
-                onClick={() => {
-                  selectScale("medium");
-                }}
-                primaryText="Medium"
-                value="medium"
-                rightIcon={
-                  selectedScale === "medium" ? <NavigationCheck /> : null
-                }
-              />,
-              <MenuItem
-                onClick={() => {
-                  selectScale("small");
-                }}
-                primaryText="Small"
-                value="small"
-                rightIcon={
-                  selectedScale === "small" ? <NavigationCheck /> : null
-                }
-              />,
-              <MenuItem
-                onClick={() => {
-                  selectScale("tiny");
-                }}
-                primaryText="Tiny"
-                value="tiny"
-                rightIcon={
-                  selectedScale === "tiny" ? <NavigationCheck /> : null
-                }
-              />,
-            ]}
-          />
-        </Menu>
-      </Popover>
-    </div>
-  );
+const capitalizeWord = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 };
 
-// Options.propTypes = {
-//   optionsOpen: PropTypes.bool.isRequired,
-//   openOptions: PropTypes.func.isRequired,
-//   closeOptions: PropTypes.func.isRequired,
-//   optionsAnchorElement: PropTypes.object.isRequired,
-//   selectedScale: PropTypes.string.isRequired,
-//   selectScale: PropTypes.func.isRequired,
-//   changeView: PropTypes.func.isRequired,
-//   view: PropTypes.string.isRequired
-// }
+function ViewMenu() {
+  const { view } = useBurwellState((state) => state);
+  const runAction = useBurwellActions();
+
+  const views = ["map", "list"];
+  return h(MenuItem, { text: "View" }, [
+    views.map((v, i) => {
+      const onClick = (e) => {
+        runAction({ type: "change-view", view: v });
+      };
+
+      const iconName = v === view ? "tick" : null;
+      const intent = v === view ? "primary" : "none";
+      return h(MenuItem, {
+        onClick,
+        intent,
+        key: i,
+        text: capitalizeWord(v),
+        labelElement: h(Icon, { icon: iconName }),
+      });
+    }),
+  ]);
+}
+
+function ScaleMenu() {
+  const { selectedScale } = useBurwellState((state) => state);
+  const runAction = useBurwellActions();
+
+  const sizes = ["all", "large", "medium", "small", "tiny"];
+  return h(MenuItem, { text: "Scale" }, [
+    sizes.map((size, i) => {
+      const onClick = (e) => {
+        runAction({ type: "select-scale", selectedScale: size });
+      };
+      const iconName = size === selectedScale ? "tick" : null;
+      const intent = size === selectedScale ? "primary" : "none";
+      return h(MenuItem, {
+        onClick,
+        intent,
+        key: i,
+        text: capitalizeWord(size),
+        labelElement: h(Icon, { icon: iconName }),
+      });
+    }),
+  ]);
+}
+
+function OptionMenu() {
+  return h(Menu, [h(ViewMenu), h(ScaleMenu)]);
+}
+
+function Options() {
+  return h(Popover, { content: h(OptionMenu), minimal: true }, [
+    h(Button, { minimal: true }, [
+      h("h3", { style: { margin: "0" } }, ["Options"]),
+    ]),
+  ]);
+}
 
 export default Options;
