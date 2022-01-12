def74d1c67803fda7b18995933c2bfffadd9072b
Casey Idzikowski <idzikoj1234@gmail.com>
2022-01-12 13:54:25 -0600

    unify info components

--- src/burwell-sources/components/feature-list.tsx
+++ src/burwell-sources/components/feature-list.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import {
-  useBurwellState,
-  getVisibleScale,
-  useBurwellActions,
-} from "~/burwell-sources/app-state";
+import { useBurwellActions } from "~/burwell-sources/app-state";
 import { ExpansionPanel } from "~/map-interface/components/expansion-panel";
 import h from "@macrostrat/hyper";
+import { settings, zoomMap } from "../app-state/utils";
 
 function FeatureTable(props) {
   const { d } = props;
@@ -40,54 +37,69 @@ function FeatureTable(props) {
   );
 }
 
-const FeatureList = () => {
-  const runAction = useBurwellActions();
-  const { selectedScale, maps } = useBurwellState((state) => state);
+function ViewMap({ feature }) {
+  return (
+    <a
+      href={
+        settings.uri +
+        "/burwell#" +
+        zoomMap[feature.properties.scale] +
+        "/" +
+        (feature.geometry.coordinates[0][0][1] +
+          feature.geometry.coordinates[0][2][1]) /
+          2 +
+        "/" +
+        (feature.geometry.coordinates[0][0][0] +
+          feature.geometry.coordinates[0][2][0]) /
+          2
+      }
+      target="_blank"
+    >
+      View map
+    </a>
+  );
+}
 
+function FeatureContent({ d, activateFeature }) {
+  return (
+    <div
+      onMouseEnter={() => activateFeature(d)}
+      onMouseOut={() => activateFeature({})}
+    >
+      <p>
+        <span className="map-source-title">{d.properties.ref_title}</span>
+        {d.properties.authors} ({d.properties.ref_year}).{" "}
+        <i>{d.properties.ref_title}</i>. {d.properties.ref_source}.
+        {d.properties.isbn_doi}. Retrieved from{" "}
+        <a href={d.properties.url} target="_blank">
+          {d.properties.url}
+        </a>
+        .{" "}
+      </p>
+      <FeatureTable d={d} />
+      <ViewMap feature={d} />
+    </div>
+  );
+}
+
+const FeatureList = ({ features }) => {
+  const runAction = useBurwellActions();
   const activateFeature = (feature) => {
     runAction({ type: "activate-feature", activeFeature: feature });
   };
 
-  const data = getVisibleScale(maps, selectedScale);
-
   return (
     <div className="feature-list-container">
-      {data.map((d, i) => {
+      {features.map((d, i) => {
         return h(
           ExpansionPanel,
           { title: d.properties.name, expanded: true, key: i },
-          [
-            <div className="eight columns">
-              <p
-                onMouseEnter={() => activateFeature(d)}
-                onMouseOut={() => activateFeature({})}
-              >
-                {d.properties.authors} ({d.properties.ref_year}).{" "}
-                <i
-                  onMouseEnter={() => activateFeature(d)}
-                  onMouseOut={() => activateFeature({})}
-                >
-                  {d.properties.ref_title}
-                </i>
-                . {d.properties.ref_source}.{d.properties.isbn_doi}. Retrieved
-                from{" "}
-                <a
-                  href={d.properties.url}
-                  target="_blank"
-                  onMouseEnter={() => activateFeature(d)}
-                  onMouseOut={() => activateFeature({})}
-                >
-                  {d.properties.url}
-                </a>
-                .{" "}
-              </p>
-              <FeatureTable d={d} />
-            </div>,
-          ]
+          [<FeatureContent d={d} activateFeature={activateFeature} />]
         );
       })}
     </div>
   );
 };
 
+export { FeatureContent };
 export default FeatureList;
