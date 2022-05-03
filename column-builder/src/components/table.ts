c1942bb6ccfafdbef23fe42140fdaa1e8bb14fa1
Casey Idzikowski <idzikoj1234@gmail.com>
2022-05-03 21:13:58 +0200

    colsection and colunit rows

--- column-builder/src/components/table.ts
+++ column-builder/src/components/table.ts
@@ -3,6 +3,9 @@ import { hyperStyled } from "@macrostrat/hyper";
 import Link from "next/link";
 import { ReactChild } from "react";
 import styles from "./comp.module.scss";
+import { UnitsView, ColSectionI } from "../types";
+import { SectionUnitCheckBox } from "./column";
+import { UnitLithHelperText } from "./unit";
 
 const h = hyperStyled(styles);
 
@@ -38,7 +41,8 @@ interface TableProps {
 }
 
 function Table(props: TableProps) {
-  const baseClass = "bp3-html-table .bp3-html-table-bordered";
+  const baseClass =
+    "bp3-html-table .bp3-html-table-condensed .bp3-html-table-bordered";
   let tableClassName = props.interactive
     ? `${baseClass} .bp3-interactive`
     : baseClass;
@@ -60,4 +64,141 @@ function TableHeader(props: { headers: any[] }) {
   ]);
 }
 
-export { Row, Table, FeatureCell, TableHeader };
+function UnitRowCellGroup(props: { unit: UnitsView }) {
+  const { unit } = props;
+
+  return h(React.Fragment, [
+    h("td", [unit.id]),
+    h("td", [
+      h("div", [
+        unit.strat_name
+          ? `${unit.strat_name.strat_name} ${unit.strat_name.rank}`
+          : unit.unit_strat_name || "unnamed",
+      ]),
+      h(UnitLithHelperText, { lith_unit: unit.lith_unit }),
+    ]),
+    h("td", [unit.name_fo]),
+    h("td", [unit.name_lo]),
+    h("td", { style: { backgroundColor: unit.color } }, [unit.color]),
+    h("td", [`${unit.min_thick} - ${unit.max_thick}`]),
+  ]);
+}
+
+function ColSectionsTable(props: {
+  headers: any[];
+  colSections: ColSectionI[];
+  onChange: (id: number) => void;
+}) {
+  const { headers, colSections, onChange } = props;
+  return h(Table, { interactive: true }, [
+    h(TableHeader, { headers }),
+    h("tbody", [
+      colSections.map((section, i) => {
+        return h(
+          Row,
+          {
+            href: `/section/${section.id}`,
+            key: i,
+          },
+          [
+            h(
+              "td",
+              {
+                onClick: (e: React.MouseEvent<HTMLTableCellElement>) =>
+                  e.stopPropagation(),
+              },
+              [
+                h(SectionUnitCheckBox, {
+                  data: section.id,
+                  onChange: onChange,
+                }),
+              ]
+            ),
+            h("td", [section.id]),
+            h("td", [section.top]),
+            h("td", [section.bottom]),
+            h("td", [
+              h(
+                "a",
+                { href: `/section/${section.id}` },
+                `view ${section.unit_count} units`
+              ),
+            ]),
+          ]
+        );
+      }),
+    ]),
+  ]);
+}
+
+function ColUnitsTable(props: { units: UnitsView[] }) {
+  const { units } = props;
+
+  const colUnitsBySections: { [section_id: number]: UnitsView[] } = {};
+  units.map((unit) => {
+    if (unit.section_id in colUnitsBySections) {
+      colUnitsBySections[unit.section_id] = [
+        ...colUnitsBySections[unit.section_id],
+        unit,
+      ];
+    } else {
+      colUnitsBySections[unit.section_id] = [unit];
+    }
+  });
+
+  const originalIdOrder: number[] = Array.from(
+    new Set<number>(units.map((unit) => unit.section_id))
+  );
+
+  console.log(originalIdOrder, colUnitsBySections);
+
+  const headers = [
+    "ID",
+    "Strat Name",
+    "Bottom Interval",
+    "Top Interval",
+    "Color",
+    "Thickness",
+  ];
+
+  return h(Table, { interactive: true }, [
+    h(TableHeader, { headers }),
+    h("tbody", [
+      originalIdOrder.map((id, i) => {
+        let units_ = colUnitsBySections[id];
+        return h(React.Fragment, [
+          h("tr", [
+            h(
+              "td",
+              {
+                colSpan: 6,
+                style: { background: "#C5CBD3", textAlign: "center" },
+              },
+              ["Section #", id]
+            ),
+          ]),
+          units_.map((unit, i) => {
+            return h(
+              Row,
+              {
+                key: i,
+                href: `/unit/${unit.id}/edit`,
+              },
+              [h(UnitRowCellGroup, { unit, key: i })]
+            );
+          }),
+        ]);
+      }),
+    ]),
+  ]);
+}
+
+export {
+  Row,
+  Table,
+  FeatureCell,
+  TableHeader,
+  UnitRowCellGroup,
+  ColSectionsTable,
+  ColUnitsTable,
+};
