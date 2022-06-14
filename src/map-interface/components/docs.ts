176dfc02568afd8dd8314764f4ac6c8371551cc1
Daven Quinn <dev@davenquinn.com>
2022-06-14 16:13:17 -0500

    Added a swatch for version info

--- src/map-interface/components/docs.ts
+++ src/map-interface/components/docs.ts
@@ -13,3 +13,14 @@ export function DocsVideo({ slug }) {
     src: `https://macrostrat-media.s3.amazonaws.com/maps/docs/${slug}.mp4`,
   });
 }
+
+export function NewSwatch({ children }) {
+  return h("span.new-swatch", children);
+}
+
+export function Version({ spec, date }) {
+  return h("h2.version", [
+    h("span.version-name", ["Version ", h("code", spec)]),
+    h("span.date", date),
+  ]);
+}
