3bac5d27312765c22a2af23e1c1f20ea1871a42e
Daven Quinn <dev@davenquinn.com>
2023-02-01 17:53:23 -0600

    Updated map documentation

--- src/map-interface/components/docs.ts
+++ src/map-interface/components/docs.ts
@@ -1,16 +1,26 @@
 import hyper from "@macrostrat/hyper";
 import styles from "./docs.module.styl";
 import { useInView } from "react-intersection-observer";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import classNames from "classnames";
 
 const h = hyper.styled(styles);
 
-export function DocsVideo({ slug, lazy = true }) {
+function DocsMediaFile({ href, lazy = true, className }) {
   const { ref, inView } = useInView({ triggerOnce: true });
   let src = null;
   if (inView || !lazy) {
-    src = `https://macrostrat-media.s3.amazonaws.com/maps/docs/${slug}.mp4`;
+    src = `https://macrostrat-media.s3.amazonaws.com/maps/docs/${href}`;
   }
 
+  if (href.endsWith(".png")) {
+    return h("img", {
+      ref,
+      src,
+      className,
+    });
+  }
   return h("video", {
     ref,
     autoPlay: true,
@@ -19,15 +29,50 @@ export function DocsVideo({ slug, lazy = true }) {
     muted: true,
     type: "video/mp4",
     src,
+    className,
   });
 }
 
-export function NewSwatch({ children }) {
-  return h("span.new-swatch", children);
+export function DocsMedia({ children, width, float = "right", ...rest }) {
+  const className = classNames({
+    [float]: float != null,
+    captioned: children != null,
+  });
+  return h("figure.documentation-figure", { style: { width }, className }, [
+    h(DocsMediaFile, rest),
+    h.if(children != null)("figcaption.caption", children),
+  ]);
+}
+
+export function DocsVideo({ slug, lazy = true, className }) {
+  const { ref, inView } = useInView({ triggerOnce: true });
+  let src = null;
+  if (inView || !lazy) {
+    src = `https://macrostrat-media.s3.amazonaws.com/maps/docs/${slug}.mp4`;
+  }
+
+  return h("video.documentation-video-standalone", {
+    ref,
+    autoPlay: true,
+    loop: true,
+    playsInline: true,
+    muted: true,
+    type: "video/mp4",
+    src,
+    className,
+  });
+}
+
+export function NewSwatch({ children, version = 0 }) {
+  return h(
+    HashLink,
+    { to: `/changelog#version-${version}`, className: "new-swatch" },
+    children
+  );
 }
 
 export function Version({ spec, date }) {
-  return h("h2.version", [
+  return h("h2.version", { id: `version-${spec}` }, [
     h("span.version-name", ["Version ", h("code", spec)]),
     h("span.date", date),
   ]);
