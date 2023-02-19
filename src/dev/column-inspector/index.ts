e2fd87867d9a3e32f96645e93dd48a7d591f5449
Daven Quinn <dev@davenquinn.com>
2023-02-19 15:06:43 -0600

    Upgrade yarn to remove typescript errors

--- src/dev/column-inspector/index.ts
+++ src/dev/column-inspector/index.ts
@@ -11,7 +11,7 @@ import { PatternProvider } from "~/_providers";
 import { useColumnNav } from "common/macrostrat-columns";
 import ModalUnitPanel from "./modal-panel";
 import { preprocessUnits } from "@macrostrat/concept-app-helpers";
-import { ColumnMapNavigator } from "common/column-map";
+import { ColumnNavigatorMap } from "@macrostrat/column-views";
 
 const ColumnTitle = (props) => {
   return h.if(props.data != null)("h1", props.data?.col_name);
@@ -43,7 +43,7 @@ function ColumnManager() {
       ]),
     ]),
     h("div.right-column", [
-      h.if(selectedUnit == null)(ColumnMapNavigator, {
+      h.if(selectedUnit == null)(ColumnNavigatorMap, {
         className: "column-map",
         currentColumn: columnFeature,
         setCurrentColumn,
