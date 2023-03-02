ec5e0c33d476d4869bb4bd8a528a728a69339bc1
Daven Quinn <dev@davenquinn.com>
2023-03-02 05:05:57 -0600

    A basic color scheme

--- src/dev/color-schemes.ts
+++ src/dev/color-schemes.ts
@@ -18,22 +18,48 @@ const newStyle = {
             "case",
             [
               "all",
-              ["==", ["to-number", ["get", "best_age_top"], 1], 0],
+              ["<", ["to-number", ["get", "best_age_top"], 1], 0.0001],
               ["==", ["get", "lith_class1"], "sedimentary"],
             ],
-            "yellow",
+            "#FFF6BD", // yellow
             [
               "all",
               ["<", ["to-number", ["get", "best_age_top"], 1], 0.02],
               ["==", ["get", "lith_class1"], "sedimentary"],
             ],
-            "goldenrod",
+            "#FFD4B2", // light orange
             ["==", ["get", "lith_class1"], "igneous"],
-            ["case", ["==", ["get", "lith_type2"], "volcanic"], "red", "pink"],
+            [
+              "case",
+              [
+                "any",
+                ["==", ["get", "lith_type1"], "volcanic"],
+                ["==", ["get", "lith_type2"], "volcanic"],
+              ],
+              [
+                "case",
+                ["<", ["to-number", ["get", "best_age_bottom"], 24], 1],
+                "#CD0404",
+                ["<", ["to-number", ["get", "best_age_bottom"], 24], 30],
+                "red",
+                ["<", ["to-number", ["get", "best_age_bottom"], 24], 500],
+                "#EB455F", // magenta
+                "#850000", // pink
+              ],
+              "#F56EB3", // pink
+            ],
             ["==", ["get", "lith_class1"], "sedimentary"],
-            "blue",
+            //["get", "color"],
+            [
+              "case",
+              ["==", ["get", "lith_type1"], "carbonate"],
+              "#82AAE3",
+              "#91D8E4",
+            ],
+
+            //"", // green
             ["==", ["get", "lith_class1"], "metamorphic"],
-            "green",
+            "#460C68", // purple
             "#aaa",
           ],
         },
