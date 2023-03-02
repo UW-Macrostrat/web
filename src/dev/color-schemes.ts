887b2cd9825463c9ed495832615a13a16729fe75
Daven Quinn <dev@davenquinn.com>
2023-03-02 03:09:44 -0600

    Fairly cartoonish initial attempt at lithology-driven styling

--- src/dev/color-schemes.ts
+++ src/dev/color-schemes.ts
@@ -2,6 +2,46 @@ import { VectorMapInspectorPage, MacrostratVectorTileset } from "./map";
 import h from "@macrostrat/hyper";
 import { useState } from "react";
 import { ButtonGroup, Button } from "@blueprintjs/core";
+import { buildMacrostratStyle } from "~/map-interface/map-page/map-style";
+
+const baseStyle = buildMacrostratStyle();
+
+const newStyle = {
+  ...baseStyle,
+  layers: baseStyle.layers.map((layer) => {
+    if (layer.id == "burwell_fill") {
+      return {
+        ...layer,
+        paint: {
+          ...layer.paint,
+          "fill-color": [
+            "case",
+            [
+              "all",
+              ["==", ["to-number", ["get", "best_age_top"], 1], 0],
+              ["==", ["get", "lith_class1"], "sedimentary"],
+            ],
+            "yellow",
+            [
+              "all",
+              ["<", ["to-number", ["get", "best_age_top"], 1], 0.02],
+              ["==", ["get", "lith_class1"], "sedimentary"],
+            ],
+            "goldenrod",
+            ["==", ["get", "lith_class1"], "igneous"],
+            ["case", ["==", ["get", "lith_type2"], "volcanic"], "red", "pink"],
+            ["==", ["get", "lith_class1"], "sedimentary"],
+            "blue",
+            ["==", ["get", "lith_class1"], "metamorphic"],
+            "green",
+            "#aaa",
+          ],
+        },
+      };
+    }
+    return layer;
+  }),
+};
 
 export function MapColorsInspector() {
   const [tileset, setTileset] = useState(MacrostratVectorTileset.CartoSlim);
@@ -25,6 +65,7 @@ export function MapColorsInspector() {
     {
       title: "Map colors",
       tileset,
+      overlayStyle: newStyle,
     },
     [
       h("p", "Testing environment for new map coloring strategies."),
