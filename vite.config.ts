8143552cf3055263157f1b8570dd77c8b15ba139
Daven Quinn <dev@davenquinn.com>
2023-08-24 02:31:52 -0500

    Fixed searchbar styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { UserConfig } from "vite";
 import path from "path";
 import mdx from "@mdx-js/rollup";
+import revisionInfo from "@macrostrat/revision-info-webpack";
+
+import pkg from "./package.json";
+
+const gitEnv = revisionInfo(pkg, "https://github.com/UW-Macrostrat/web");
+// prefix with VITE_ to make available to client
+for (const [key, value] of Object.entries(gitEnv)) {
+  process.env["VITE_" + key] = value;
+}
 
 const config: UserConfig = {
   cacheDir: ".vite",
