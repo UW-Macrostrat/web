bf5a38a68ddfdaf8dca3301eb2913df7ee31e427
Daven Quinn <dev@davenquinn.com>
2023-09-22 09:01:07 -0500

    Map now works OK

--- src/pages/maps/@id/map-interface.ts
+++ src/pages/maps/@id/map-interface.ts
@@ -4,7 +4,7 @@ import {
   MapView,
   PanelCard,
 } from "@macrostrat/map-interface";
-import { Spinner } from "@blueprintjs/core";
+import { Spinner, Radio, RadioGroup } from "@blueprintjs/core";
 import { SETTINGS } from "~/map-interface/settings";
 import { useMapRef } from "@macrostrat/mapbox-react";
 import { useEffect } from "react";
@@ -16,6 +16,7 @@ import boundingBox from "@turf/bbox";
 import { LngLatBoundsLike } from "mapbox-gl";
 import { buildMacrostratStyle } from "@macrostrat/mapbox-styles";
 import { getMapboxStyle, mergeStyles } from "@macrostrat/mapbox-utils";
+import { useDarkMode } from "@macrostrat/ui-components";
 
 const h = hyper.styled(styles);
 
@@ -23,6 +24,8 @@ async function buildStyle({ style, mapboxToken, focusedMap }) {
   const mapStyle = buildMacrostratStyle({
     tileserverDomain: SETTINGS.burwellTileDomain,
     focusedMap,
+    fillOpacity: style == null ? 0.8 : 0.4,
+    strokeOpacity: style == null ? 0.8 : 0.2,
   });
   if (style == null) {
     return mapStyle;
@@ -40,8 +43,26 @@ function ensureBoxInGeographicRange(bounds: LngLatBoundsLike) {
   return bounds;
 }
 
+enum Basemap {
+  Satellite = "satellite",
+  Basic = "basic",
+  None = "none",
+}
+
+function basemapStyle(basemap, inDarkMode) {
+  switch (basemap) {
+    case Basemap.Satellite:
+      return SETTINGS.satelliteMapURL;
+    case Basemap.Basic:
+      return inDarkMode ? SETTINGS.darkMapURL : SETTINGS.baseMapURL;
+    case Basemap.None:
+      return null;
+  }
+}
+
 export default function MapInterface({ map }) {
   const [isOpen, setOpen] = useState(false);
+  const dark = useDarkMode()?.isEnabled ?? false;
   const title = h([
     h("code", map.properties.source_id),
     " ",
@@ -52,16 +73,18 @@ export default function MapInterface({ map }) {
     return ensureBoxInGeographicRange(boundingBox(map.geometry));
   }, [map.geometry]);
 
+  const [layer, setLayer] = useState(Basemap.None);
+
   const [mapStyle, setMapStyle] = useState(null);
   useEffect(() => {
     buildStyle({
-      style: "mapbox://styles/mapbox/satellite-v9",
+      style: basemapStyle(layer, dark),
       mapboxToken: SETTINGS.mapboxAccessToken,
       focusedMap: map.properties.source_id,
     }).then((style) => {
       setMapStyle(style);
     });
-  }, [map.properties.source_id]);
+  }, [map.properties.source_id, layer, dark]);
 
   const maxBounds: LatLngBoundsLike = useMemo(() => {
     const dx = bounds[2] - bounds[0];
@@ -83,7 +106,7 @@ export default function MapInterface({ map }) {
     {
       className: "single-map",
       navbar: h(MapNavbar, { title, parentRoute: "/maps", isOpen, setOpen }),
-      contextPanel: h(PanelCard, []),
+      contextPanel: h(PanelCard, [h(BaseLayerSelector, { layer, setLayer })]),
       contextPanelOpen: isOpen,
     },
     [
@@ -102,4 +125,24 @@ export default function MapInterface({ map }) {
       ),
     ]
   );
+}
+
+function BaseLayerSelector({ layer, setLayer }) {
+  return h("div.base-layer-selector", [
+    h("h3", "Base layer"),
+    h(
+      RadioGroup,
+      {
+        selectedValue: layer,
+        onChange(e) {
+          setLayer(e.currentTarget.value);
+        },
+      },
+      [
+        h(Radio, { label: "Satellite", value: Basemap.Satellite }),
+        h(Radio, { label: "Basic", value: Basemap.Basic }),
+        h(Radio, { label: "None", value: Basemap.None }),
+      ]
+    ),
+  ]);
 }
