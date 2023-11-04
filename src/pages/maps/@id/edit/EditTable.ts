db1c804ff22379ee11bc4f405698c07b8bb20fbc
Cannon Lock <clock@wisc.edu>
2023-11-04 18:38:02 -0500

    Add in Filtering

--- src/pages/maps/@id/edit/EditTable.ts
+++ src/pages/maps/@id/edit/EditTable.ts
@@ -1,181 +1,231 @@
 
+// @ts-ignore
 import hyper from "@macrostrat/hyper";
 
-import { ReactElement, ReactFragment, useState, useEffect, useMemo } from "react";
+import {useState, useEffect, useLayoutEffect, useRef} from "react";
 import { HotkeysProvider, InputGroup, Button } from "@blueprintjs/core";
 import { Spinner } from "@blueprintjs/core";
-import { Column, Table2, EditableCell2, RowHeaderCell2, SelectionModes } from "@blueprintjs/table";
-import {TablePagination} from "@mui/material"
+import { Column, Table2, EditableCell2, RowHeaderCell2, ColumnHeaderCell2, SelectionModes } from "@blueprintjs/table";
+
+import TableMenu from "./TableMenu";
 
 import "@blueprintjs/table/lib/css/table.css";
+import "@blueprintjs/select/lib/css/blueprint-select.css";
+
+import "./override.sass"
 import styles from "./editTable.module.sass";
+import {Filters, OperatorQueryParameter} from "./table";
+import {buildURL, Filter} from "./table-util.ts";
 
 
 const h = hyper.styled(styles);
 
 
 
-const range = (start, stop, step = 1) =>
-  Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
+const range = (start: number, stop: number, step = 1) =>
+		Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
 
 
 interface Selection {
-  cols: number[];
-  rows: number[];
+	cols: number[];
+	rows: number[];
 }
 
-class Filter {
-  constructor(column_name: string, operator: string, value: string){
-    this.column_name = column_name
-    this.operator = operator
-    this.value = value
-  }
-  to_object = () => {
-    let o = {}
-    o[this.column_name] = this.operator + "." + this.value
-    return o
-  }
 
-  to_array = () => {
-    return [this.column_name, this.operator + "." + this.value]
-  }
 
-}
-
-interface Filters {
-  [key: string]: Filter;
-}
 
 interface TableSelection {
-  columns: string[];
-  filters: Filters
+	columns: string[];
+	filter: Filter;
 }
 
 
-export default function EditTable({url}){
-
-  // Table values
-  const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(999999);
-  const [totalCount, setTotalCount] = useState(0);
-
-  const [data, setData] = useState(undefined);
-  const [dataToggle, setDataToggle] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [tableSelection, setTableSelection] = useState<TableSelection>({columns: [], filters: []})
-
-  const cellRenderer = ({key, row, cell}) => {
-
-    return h(EditableCell2, {onChange: (e) => console.log(), "value": data[row][key]}, [])
-  }
-
-  let getData = async () => {
-
-    let dataURL = new URL(url)
-
-    dataURL.searchParams.append("page", page.toString())
-    dataURL.searchParams.append("page_size", pageSize.toString())
-
-    let response = await fetch(dataURL)
-    let data = await response.json()
-
-    setTotalCount(Number.parseInt(response.headers.get("X-Total-Count")))
-    setData(data)
-  }
-
-  useEffect(() => {
-    getData()
-  }, [page, pageSize, dataToggle])
 
 
-  if(data == undefined){
-    return h(Spinner)
-  }
+interface EditTableProps {
+	url: string;
+}
 
-  const columns = Object.keys(data[0]).filter(x => x != "db_id").map((key) => {
-    return h(Column, {name: key, cellRenderer: (row, cell) => cellRenderer({"key": key, "row": row, "cell": cell}), "key": key})
-  })
+export default function EditTable({url}: EditTableProps){
 
-  const getSelectionValues = (selections: Selection[]) => {
+	const [data, setData] = useState<any[]>([]);
+	const [inputValue, setInputValue] = useState<string>("");
+	const [error, setError] = useState<string | undefined>(undefined)
+	const [filters, _setFilters] = useState<Filters>({})
+	const [tableSelection, _setTableSelection] = useState<TableSelection>({columns: [], filter: new Filter("db_id", "in", "")})
+	const [valueSelection, setValueSelection] = useState<string[]>([])
 
-    if(selections.length == 0){
-      setTableSelection({columns: [], filters: {...tableSelection.filters, "tableSelection": undefined}})
-      return
-    }
+	const setFilters = (filters: Filters) => {
+		_setFilters(filters)
+	}
 
-    const rows = selections[0]?.rows
-    const cols = selections[0]?.cols
-
-    const columnsKeys = Object.keys(data[0])
-    const selectedColumnKeys = columnsKeys.slice(cols[0], cols[1] + 1)
-
-    let selection: TableSelection
-    if(rows == undefined){
-      selection = {columns: selectedColumnKeys, ...tableSelection}
-
-    } else {
-      const selectedRowIndices = rows != undefined ? range(rows[0], rows[1] + 1) : range(0, data.length)
-      const dbIds = selectedRowIndices.map((row) => data[row]['db_id'])
-      const filter = new Filter("db_id", "in", "(" + dbIds.join(",") + ")")
+	const setTableSelection = (selection: TableSelection) => {
+		_setTableSelection(selection)
+	}
 
 
-      selection = {columns: selectedColumnKeys, filters: {...tableSelection.filters, "tableSelection": filter}}
-    }
+	const columnHeaderCellRenderer = (columnIndex: number) => {
 
-    setTableSelection(selection)
-  }
+		const columnName: string = Object.keys(data[0])[columnIndex]
 
-  const rowHeaderCellRenderer = (rowIndex: number) => {
-    return h(RowHeaderCell2, {name: data[rowIndex]['db_id']}, [])
-  }
+		const onChange = (param: OperaterQueryParameter) => {
 
-  const submitChange = async (value: string) => {
-    for (const column of tableSelection.columns) {
+			const columnFilter = new Filter(columnName, param.operator, param.value)
 
-      let updateURL = new URL(url)
+			setFilters({...filters, [columnName]: columnFilter})
+		}
 
-      for(const filter: Filter of Object.values(tableSelection.filters)){
-        updateURL.searchParams.append(...filter.to_array())
-      }
-
-      let patch =  {[column]: value}
-      console.log(patch, JSON.stringify(patch))
+		let filter = filters[columnName]
 
 
-      let response = await fetch(updateURL, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(patch),
-      })
+		return h(ColumnHeaderCell2, {
+			menuRenderer: () => h(TableMenu, {"onChange": onChange, filter}),
+			name: columnName
+		}, [])
+	}
 
-      if(response.status != 204){
-        console.error("Failed to update", response)
-      }
-    }
-    setDataToggle(!dataToggle)
-  }
+	const cellRenderer = ({key, row, cell}) => {
+
+		return h(EditableCell2, {onChange: (e) => console.log(), "value": data[row][key]}, [])
+	}
+
+	let getData = async () => {
+
+		const dataURL = buildURL(url, Object.values(filters))
+
+		const response = await fetch(dataURL)
+		const newData = await response.json()
+
+		if(newData.length == 0){
+			setError("Warning: No results matched query")
+		} else {
+			console.log("Data fetched successfully")
+
+			setError(undefined)
+			setData(newData)
+		}
+
+		return newData
+	}
+
+	const dataFetched = useRef(false)
+	useLayoutEffect(() => {
+		if(!dataFetched.current){
+			dataFetched.current = true
+			return
+		}
+		getData()
+	}, [tableSelection, filters])
 
 
-  return h(HotkeysProvider, {}, [
-    h("div.table-container", {}, [
-      h("div.input-form", {}, [
-        h(InputGroup, {"value": inputValue, className: "update-input-group", onChange: (e) => setInputValue(e.target.value)}),
-        h(Button, {type: "submit", onClick: () => submitChange(inputValue)}, ["Submit"])
-      ]),
-      h(Table2,
-        {
-          selectionModes: SelectionModes.COLUMNS_AND_CELLS,
-          rowHeaderCellRenderer: rowHeaderCellRenderer,
-          onSelection: (selections: Selection[]) => getSelectionValues(selections),
-          numRows: data.length
-        },
-        [
-          columns
-        ]
-      ),
-      h(TablePagination, {component: "div", count: totalCount, rowsPerPage: pageSize, page: page, onRowsPerPageChange: (e) => setPageSize(e.target.value), onPageChange: (e, p) => setPage(p)}),
-    ])
-  ])
+	useEffect(() => {
+		(async function () {
+			let data = await getData()
+			let newFilters: Filters = Object.keys(data[0]).reduce((original, key) => {
+				let originalFilters: Filters = {...original}
+				originalFilters[key] = new Filter(key, undefined, "")
+				return originalFilters
+			}, {})
+			setFilters({...filters, ...newFilters})
+		}());
+	}, [])
+
+	if(data.length == 0 && error == undefined){
+		return h(Spinner)
+	}
+
+
+	const getSelectionValues = (selections: Selection[]) => {
+
+		if(selections.length == 0){
+			setTableSelection({columns: [], filter: new Filter("db_id", "in", "")})
+			return
+		}
+
+		const rows = selections[0]?.rows
+		const cols = selections[0]?.cols
+
+		const columnsKeys = Object.keys(data[0])
+		const selectedColumnKeys: string[] = columnsKeys.slice(cols[0], cols[1] + 1)
+		const selectedRowIndices: number[] = rows != undefined ? range(rows[0], rows[1] + 1) : range(0, data.length)
+
+		let selection: TableSelection
+		if(rows == undefined){
+
+			selection = {filter: new Filter("db_id", "in", ""), columns: selectedColumnKeys}
+
+		} else {
+			const dbIds = selectedRowIndices.map((row) => data[row]['db_id'])
+			const filter = new Filter("db_id", "in", "(" + dbIds.join(",") + ")")
+			selection = {columns: selectedColumnKeys, "filter": filter}
+		}
+
+		let valueSelection: string[] = []
+		for(const column of selectedColumnKeys){
+			for(const row of selectedRowIndices){
+				valueSelection.push(data[row][column])
+			}
+		}
+
+		setValueSelection(valueSelection)
+		setTableSelection(selection)
+	}
+
+	const rowHeaderCellRenderer = (rowIndex: number) => {
+		return h(RowHeaderCell2, {name: data[rowIndex]['db_id']}, [])
+	}
+
+
+
+	const submitChange = async (value: string) => {
+		for (const column of tableSelection.columns) {
+
+			let updateURL = buildURL(url, [tableSelection.filter, ...Object.values(filters)])
+
+			let patch =  {[column]: value}
+
+
+			let response = await fetch(updateURL, {
+				method: "PATCH",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(patch),
+			})
+
+			if(response.status != 204){
+				console.error("Failed to update", response)
+			}
+		}
+		getData()
+	}
+
+
+	return h(HotkeysProvider, {}, [
+		h("div.table-container", {}, [
+			h.if(error)("div.warning", {}, [error]),
+			h("div.input-form", {}, [
+				h(InputGroup, {"value": inputValue, className: "update-input-group", onChange: (e) => setInputValue(e.target.value)}),
+				h(Button, {type: "submit", onClick: () => submitChange(inputValue)}, ["Submit"])
+			]),
+			h(Table2,
+					{
+						selectionModes: SelectionModes.COLUMNS_AND_CELLS,
+						rowHeaderCellRenderer: rowHeaderCellRenderer,
+						onSelection: (selections: Selection[]) => getSelectionValues(selections),
+						numRows: data.length
+					},
+					[
+							...Object.keys(data[0]).filter(x => x != "db_id").map((key) => {
+							return h(Column, {
+								name: key,
+								columnHeaderCellRenderer: columnHeaderCellRenderer,
+								cellRenderer: (row, cell) => cellRenderer({"key": key, "row": row, "cell": cell}),
+								"key": key
+							})
+						})
+					]
+			)
+		])
+	])
 }
---
+++ src/pages/maps/@id/edit/TableMenu.ts
@@ -0,0 +1,112 @@
+import {Button, Menu, MenuItem, InputGroup} from "@blueprintjs/core";
+import {Select2, ItemRenderer} from "@blueprintjs/select";
+import React from "react";
+import {useDebouncedCallback} from "use-debounce";
+
+// @ts-ignore
+import hyper from "@macrostrat/hyper";
+
+import {OperatorQueryParameter, ColumnOperatorOption} from "./table";
+
+import "@blueprintjs/core/lib/css/blueprint.css"
+import "@blueprintjs/select/lib/css/blueprint-select.css";
+import styles from "./editTable.module.sass";
+import {Filter} from "./table-util.ts";
+
+
+const h = hyper.styled(styles);
+
+
+const validExpressions: ColumnOperatorOption[] = [
+	{key: "eq", value: "=", verbose: "Equals"},
+	{key: "lt", value: "<", verbose: "Is less than"},
+	{key: "le", value: "<=", verbose: "Is less than or equal to"},
+	{key: "gt", value: ">", verbose: "Is greater than"},
+	{key: "ge", value: ">=", verbose: "Is greater than or equal to"},
+	{key: "ne", value: "<>", verbose: "Is not equal to"},
+	{key: "like", value: "LIKE", verbose: "Like"},
+	{key: "is", value: "IS", verbose: "Is", placeholder: "true | false | null"},
+	{key: "in", value: "IN", verbose: "In", placeholder: "1,2,3"}
+]
+
+
+
+const OperatorFilterOption: ItemRenderer<ColumnOperatorOption> = (column, { handleClick, handleFocus, modifiers }) => {
+
+	return h(MenuItem, {
+		shouldDismissPopover: false,
+		active: modifiers.active,
+		disabled: modifiers.disabled,
+		key: column.key,
+		label: column.verbose,
+		onClick: handleClick,
+		onFocus: handleFocus,
+		text: column.value,
+		roleStructure:"listoption"
+	}, [])
+}
+
+interface TableMenuProps {
+	onChange: (query: OperatorQueryParameter) => void;
+	filter: Filter;
+}
+
+const TableMenu = ({onChange, filter} : TableMenuProps) => {
+
+	const [menuOpen, setMenuOpen] = React.useState<boolean>(false);
+	const [inputPlaceholder, setInputPlaceholder] = React.useState<string>("");
+
+	// Create a debounced version of the text state
+	const [inputValue, setInputValue] = React.useState<string>(filter.value);
+	const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		console.log("Its morphin time!")
+		setMenuOpen(false);
+		onChange({operator: filter.operator, value: e.target.value})
+	}
+	const debouncedInputChange = useDebouncedCallback(onInputChange, 1000);
+
+	// Set the expression current value from the parent filter
+	const selectedExpression = validExpressions.find((expression) => expression.key === filter.operator);
+
+	return h(Menu, {}, [
+		h("div.filter-container", {}, [
+			h("div.filter-header", {}, ["Filter"]),
+			h("div.filter-select", {}, [
+				h(Select2<ColumnOperatorOption>, {
+					fill: true,
+					items: validExpressions,
+					className: "update-input-group",
+					filterable: false,
+					popoverProps: {isOpen: menuOpen},
+					itemRenderer: OperatorFilterOption,
+					onItemSelect: (operator: ColumnOperatorOption) => {
+						setMenuOpen(false);
+						setInputPlaceholder(operator.placeholder || "");
+						onChange({operator: operator.key, value: filter.value})
+					},
+					noResults: h(MenuItem, {disabled: true, text: "No results.", roleStructure: "listoption"}, []),
+				}, [
+					h(Button, {
+						fill: true,
+						onClick: () => setMenuOpen(!menuOpen),
+						alignText: "left",
+						text: selectedExpression?.verbose,
+						rightIcon: "double-caret-vertical",
+						className: "update-input-group",
+						placeholder: "Select A Filter"
+					}, [])
+				]),
+			]),
+			h("div.filter-input", {}, [
+				h(InputGroup, {
+					"value": inputValue,
+					className: "update-input-group",
+					placeholder: inputPlaceholder,
+					onChange: (e: React.ChangeEvent<HTMLInputElement>) => {setInputValue(e.target.value); debouncedInputChange(e)}
+				}, [])
+			])
+		])
+	])
+}
+
+export default TableMenu;
