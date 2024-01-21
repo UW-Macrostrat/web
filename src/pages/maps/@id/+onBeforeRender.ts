63e5b2c403c79eb419c33da71d397ef113e2a368
Daven Quinn <code@davenquinn.com>
2024-01-21 17:46:06 -0600

    Fix error with maps page rendering, caused by #121

---
+++ src/pages/maps/@id/+onBeforeRender.ts
@@ -0,0 +1,28 @@
+import { PageContextBuiltInServer } from "vike/types";
+import { apiV2Prefix } from "~/settings";
+
+const apiAddress = apiV2Prefix + "/defs/sources";
+
+export async function onBeforeRender(pageContext: PageContextBuiltInServer) {
+  const { id } = pageContext.routeParams;
+
+  const params = new URLSearchParams({
+    format: "geojson",
+    source_id: id,
+  });
+  const response = await fetch(apiAddress + "?" + params);
+  const data: any = await response.json();
+  const map = data?.success?.data?.features[0];
+
+  return {
+    pageContext: {
+      pageProps: {
+        map,
+      },
+      documentProps: {
+        // The page's <title>
+        title: map.properties.name,
+      },
+    },
+  };
+}
