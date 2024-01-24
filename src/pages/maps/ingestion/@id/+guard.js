41e6bbb0f0b57854cdd40816fa6feb1b7127847b
Cannon Lock <clock@wisc.edu>
2024-01-24 14:10:00 -0600

    Move the guard outside of the route file

---
+++ src/pages/maps/ingestion/@id/+guard.js
@@ -0,0 +1,24 @@
+import { render, redirect } from "vike/abort";
+
+const guard = (pageContext) => {
+  const { user } = pageContext;
+
+  if (user === undefined) {
+    // Render the login page while preserving the URL. (This is novel technique
+    // which we explain down below.)
+    throw redirect(
+      `${
+        import.meta.env.VITE_MACROSTRAT_INGEST_API
+      }/security/login?return_url=${pageContext.url}`
+    );
+    /* The more traditional way, redirect the user:
+    throw redirect('/login')
+    */
+  }
+  if (!user.groups.includes(1)) {
+    // Render the error page and show message to the user
+    return render(403, "Only admins are allowed to access this page.");
+  }
+};
+
+export default guard;
