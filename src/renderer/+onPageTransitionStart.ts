440102a752041a30fdc0325b3b04fed76073eba6
Daven Quinn <code@davenquinn.com>
2024-02-19 11:56:51 -0600

    Added basic page transition classes

---
+++ src/renderer/+onPageTransitionStart.ts
@@ -0,0 +1,10 @@
+export { onPageTransitionStart };
+
+async function onPageTransitionStart() {
+  // This function is called when a page transition starts.
+  // You can use it to add your own logic.
+  // For example, you can track page views with Google Analytics.
+  // See https://vite-plugin-ssr.com/page-transitions
+  console.log("Page transition start");
+  document.querySelector("body").classList.add("in-page-transition");
+}
