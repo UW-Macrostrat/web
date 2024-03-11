4ecf460121dab806b979ebf1e518833ded834fc8
Cannon Lock <clock@wisc.edu>
2024-03-11 13:52:11 -0500

    Don't guard in dev

--- src/pages/maps/ingestion/@id/+guard.js
+++ src/pages/maps/ingestion/@id/+guard.js
@@ -3,20 +3,23 @@ import { redirect, render } from "vike/abort";
 export const guard = (pageContext) => {
   const { user } = pageContext;
 
-  if (user === undefined) {
-    // Render the login page while preserving the URL. (This is novel technique
-    // which we explain down below.)
-    throw redirect(
-      `${
-        import.meta.env.VITE_MACROSTRAT_INGEST_API
-      }/security/login?return_url=${pageContext.url}`
-    );
-    /* The more traditional way, redirect the user:
-    throw redirect('/login')
-    */
-  }
-  if (!user.groups.includes(1)) {
-    // Render the error page and show message to the user
-    throw render(403, "Only admins are allowed to access this page.");
+  // Check that we are in production mode
+  if (import.meta.env.NODE_ENV === "production") {
+    if (user === undefined) {
+      // Render the login page while preserving the URL. (This is novel technique
+      // which we explain down below.)
+      throw redirect(
+        `${
+          import.meta.env.VITE_MACROSTRAT_INGEST_API
+        }/security/login?return_url=${pageContext.url}`
+      );
+      /* The more traditional way, redirect the user:
+      throw redirect('/login')
+      */
+    }
+    if (!user.groups.includes(1)) {
+      // Render the error page and show message to the user
+      throw render(403, "Only admins are allowed to access this page.");
+    }
   }
 };
