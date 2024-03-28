61e5789bde0d23a1d126855922bd1816a71cf5c5
Cannon Lock <clock@wisc.edu>
2024-03-28 14:41:24 -0500

    Midpoint push

---
+++ src/pages/docs/api/postgrest/+onBeforeRender.ts
@@ -0,0 +1,13 @@
+
+export async function onBeforeRender(pageContext) {
+  // `.page.server.js` files always run in Node.js; we could use SQL/ORM queries here.
+
+  const url = `${import.meta.env.VITE_MACROSTRAT_API_DOMAIN}/api/pg`
+
+  const pageProps = { swaggerUrl: url };
+  return {
+    pageContext: {
+      pageProps,
+    },
+  };
+}
