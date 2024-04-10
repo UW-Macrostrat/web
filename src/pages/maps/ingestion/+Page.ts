958dd408408ef5b45488b9157521aaf9fe28901d
Cannon Lock <clock@wisc.edu>
2024-04-10 11:44:27 -0500

    Adds Tags

--- src/pages/maps/ingestion/+Page.ts
+++ src/pages/maps/ingestion/+Page.ts
@@ -11,12 +11,14 @@ import {
   Navbar,
   AnchorButton,
   Tooltip,
-  Card,
+  Card
 } from "@blueprintjs/core";
 import { ContentPage } from "~/layouts";
 import AddButton from "~/pages/maps/ingestion/components/AddButton";
 import IngestProcessCard from "~/pages/maps/ingestion/components/IngestProcessCard";
 import { useEffect, useState, useContext } from "react";
+
+import Tag from "./components/Tag"
 
 const h = hyper.styled(styles);
 
@@ -24,15 +26,36 @@ const h = hyper.styled(styles);
 export function Page({ user, url }) {
 
   const [ingestProcess, setIngestProcess] = useState<IngestProcess[]>([])
+  const [ingestFilter, setIngestFilter] = useState<string>("")
+  const [tags, setTags] = useState<string[]>([]);
 
   useEffect(() => {
     const getIngestProcesses = async () => {
-      const response = await fetch(`${ingestPrefix}/ingest-process?source_id=order_by&source_id=not.is.null&state=eq.ingested&page_size=1000`);
+
+      // Get the query from the url
+      let urlFilter = "";
+      if(window.location.search) {
+        urlFilter = window.location.search.substring(1);
+      }
+
+
+      const response = await fetch(`${ingestPrefix}/ingest-process?source_id=order_by&source_id=not.is.null&state=eq.ingested&page_size=1000&${ingestFilter}&${urlFilter}`);
       const ingest_processes = await response.json();
       setIngestProcess(ingest_processes);
     }
     getIngestProcesses();
+  }, [ingestFilter])
+
+  useEffect(() => {
+    const getTags = async () => {
+      const response = await fetch(`${ingestPrefix}/ingest-process/tags`);
+      const tags = await response.json();
+      setTags(tags);
+    }
+    getTags();
   }, [])
+
+  console.log(ingestProcess)
 
 
   return h("div", [
@@ -59,12 +82,24 @@ export function Page({ user, url }) {
         ),
       ]),
     ]),
-    h(ContentPage, [
+    h("div",
+      { style: {display: "grid", gridTemplateColumns: "1000px", justifyContent: "center"} },
+      [h("h1", {style: {marginLeft: "7px"}}, ["Source Map Ingestion"])]
+    ),
+
+    h("div",
+      {
+        style: {display: "grid", gridTemplateColumns: "800px 200px", justifyContent: "center"}
+      },
+      [
         h("div", {style: {display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(400px, 1fr))"}}, [
           ingestProcess.map((d) => {
             return h(
               "div",
-              { style: { maxWidth: "1000px", width: "100%", margin: "auto" } },
+              {
+                key: d.id,
+                style: { maxWidth: "1000px", width: "100%", margin: "auto" }
+              },
               [
                 h(IngestProcessCard, {
                   ingestProcess: d,
@@ -72,6 +107,38 @@ export function Page({ user, url }) {
                 }),
               ]
             );
+          })
+        ]),
+        h("div", {
+          style: {
+            display: "block",
+            marginTop: "7px"
+          }
+        }, [
+          h("h3", {style: {margin: 0, marginBottom: "7px"}}, ["Tags"]),
+          h(Tag, {
+            value: "None",
+            onClick: async () => {
+              setIngestFilter("")
+              const url = new URL(window.location.origin + window.location.pathname);
+              url.searchParams.delete("tags")
+              window.history.pushState({page: "Update Search Params"}, "Title", url);
+            },
+            style: {width: "100%", marginBottom: "7px"}
+          }),
+          tags.map((tag) => {
+            return h(Tag, {
+              key: tag,
+              value: tag,
+              onClick: async () => {
+
+                const url = new URL(window.location.origin + window.location.pathname);
+                url.searchParams.set("tags", `eq.${tag}`);
+                setIngestFilter(`tags=eq.${tag}`)
+                window.history.pushState({page: "Update Search Params"}, "Title", url);
+              },
+              style: {width: "100%", marginBottom: "7px"}
+            })
           })
         ])
       ]
