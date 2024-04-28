331ae8566745a18a34698b5b10ff476b1cf689f2
Daven Quinn <code@davenquinn.com>
2024-04-28 04:58:08 -0400

    Pretty decent lithology hierarchy (SVG tree)

--- packages/lithology-hierarchy/src/index.ts
+++ packages/lithology-hierarchy/src/index.ts
@@ -2,18 +2,14 @@ import hyper from "@macrostrat/hyper";
 import styles from "./main.module.sass";
 import { apiV2Prefix } from "@macrostrat-web/settings";
 import { Spinner } from "@blueprintjs/core";
-import { ThreeColumnLayout } from "@macrostrat/ui-components";
-import {
-  useAPIResult,
-  ErrorCallout,
-  JSONView,
-} from "@macrostrat/ui-components";
+import { useAPIResult, ErrorCallout } from "@macrostrat/ui-components";
 import { useState } from "react";
-import Example, { TreeNode } from "./example";
+import { nestLiths, Lith } from "./nest-data";
+import Hierarchy from "./svg-hierarchy";
 
 const h = hyper.styled(styles);
 
-export default function MacrostratLithologyHierarchy() {
+export default function MacrostratLithologyHierarchy({ width, height }) {
   const [error, setError] = useState(null);
   const res = useAPIResult(
     `${apiV2Prefix}/defs/lithologies`,
@@ -23,125 +19,18 @@ export default function MacrostratLithologyHierarchy() {
     { onError: setError }
   );
   //if (liths.error) const data = liths?.success?.data;
-
+  console.log("Test");
   if (error != null) {
     return h(ErrorCallout, { error });
   }
   if (res == null) {
     return h(Spinner);
   }
-  const liths = res.success.data;
+  const liths: Lith[] = res.success.data;
 
   return h("div.flex.row", [
-    h(
-      "div.example-container",
-      h(Example, { width: 800, height: 2000, data: nestLiths(liths) })
-    ),
+    h("div.example-container", [
+      h(Hierarchy, { width, height, data: nestLiths(liths) }),
+    ]),
   ]);
-}
-
-interface Lith {
-  lith_id: number;
-  name: string;
-  type: string | null;
-  group: string | null;
-  class: string | null;
-  color: string;
-  fill?: number;
-}
-
-interface TreeNodeMap {
-  name: string;
-  children?: Map<string, TreeNodeMap>;
-}
-
-function nestLiths(liths: Lith[]): TreeNode {
-  const root: TreeNodeMap = { name: "All lithologies", children: new Map() };
-  // Ensure that empty strings are treated as null
-  for (let lith of liths) {
-    for (const key of ["type", "group", "class"]) {
-      if (lith[key] === "") lith[key] = null;
-    }
-  }
-
-  for (let lith of liths) {
-    if (lith.class == null || lith.type == null)
-      console.error(lith, "Class and type should never be null");
-    if (lith.class == null) console.log(lith.name, "Class is null");
-    if (lith.type == null) console.log(lith.name, "Type is null");
-
-    // Create a class if it doesn't exist
-    if (lith.class != null) {
-      if (!root.children.has(lith.class)) {
-        root.children.set(lith.class, {
-          name: lith.class,
-          children: new Map<string, TreeNodeMap>(),
-        });
-      }
-    } else {
-      if (!root.children.has(lith.name)) {
-        root.children.set(lith.name, { name: lith.name });
-      }
-    }
-
-    // Add the type to the class
-    if (lith.class != null && lith.type != null) {
-      const parent = root.children.get(lith.class);
-      if (!parent.children.has(lith.type)) {
-        parent.children.set(lith.type, {
-          name: lith.type,
-          children: new Map<string, TreeNodeMap>(),
-        });
-      }
-    }
-
-    // Add the group to the type
-    if (lith.class != null && lith.type != null) {
-      if (lith.group != null) {
-        const parent = root.children.get(lith.class);
-        const grandparent = parent.children.get(lith.type);
-        if (!grandparent.children.has(lith.group)) {
-          grandparent.children.set(lith.group, {
-            name: lith.group,
-            children: new Map<string, TreeNodeMap>(),
-          });
-        }
-      } else {
-        const parent = root.children.get(lith.class);
-        const grandparent = parent.children.get(lith.type);
-        if (!grandparent.children.has(lith.name)) {
-          grandparent.children.set(lith.name, { name: lith.name });
-        }
-      }
-    }
-
-    // Add the lithology to the group
-    if (
-      lith.class != null &&
-      lith.type != null &&
-      lith.group != null &&
-      lith.name != null
-    ) {
-      const parent = root.children.get(lith.class);
-      const grandparent = parent.children.get(lith.type);
-      const greatgrandparent = grandparent.children.get(lith.group);
-      greatgrandparent.children.set(lith.name, {
-        name: lith.name,
-        children: new Map<string, TreeNodeMap>(),
-      });
-    }
-  }
-
-  // Export to TreeNode format
-  return convert(root);
-}
-
-function convert(data: TreeNodeMap): TreeNode {
-  if (data.children == null) {
-    return { name: data.name };
-  }
-  return {
-    name: data.name,
-    children: Array.from(data.children.values()).map(convert),
-  };
 }
--- src/pages/dev/ui-tests/lithology/+Page.ts
+++ src/pages/dev/ui-tests/lithology/+Page.ts
@@ -1,8 +1,20 @@
 import LithologyHierarchy from "@macrostrat-web/lithology-hierarchy";
 import { FullscreenPage } from "~/layouts";
-import h from "@macrostrat/hyper";
+import hyper from "@macrostrat/hyper";
 import { PageBreadcrumbs } from "~/renderer";
+import styles from "./main.module.sass";
+import { useRef } from "react";
+import { useElementSize } from "@macrostrat/ui-components";
+const h = hyper.styled(styles);
 
 export function Page() {
-  return h(FullscreenPage, [h(PageBreadcrumbs), h(LithologyHierarchy)]);
+  return h(FullscreenPage, [
+    h("div.main", [h(PageBreadcrumbs), h(HierarchyContainer)]),
+  ]);
+}
+
+function HierarchyContainer() {
+  const ref = useRef(null);
+  const { width, height } = useElementSize(ref) ?? {};
+  return h("div.flex-grow", { ref }, h(LithologyHierarchy, { width, height }));
 }
