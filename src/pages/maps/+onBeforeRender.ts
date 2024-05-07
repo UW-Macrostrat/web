8d820c3a3afd20658e80537667f9b94218d5fa85
Cannon Lock <clock@wisc.edu>
2024-05-07 13:47:12 -0500

    Filter sources to is_mapped

--- src/pages/maps/+onBeforeRender.ts
+++ src/pages/maps/+onBeforeRender.ts
@@ -4,9 +4,9 @@ export async function onBeforeRender(pageContext) {
   // `.page.server.js` files always run in Node.js; we could use SQL/ORM queries here.
 
   const res = await postgrest
-    .from("sources_metadata")
+    .from("sources")
     .select("*")
-    .order("source_id", { ascending: true });
+    .order("source_id", { ascending: true })
 
   const pageProps = { sources: res.data };
   return {
