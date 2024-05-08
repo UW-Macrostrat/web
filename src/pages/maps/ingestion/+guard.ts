a66f875eacdab17ec78f6629702f3d467a5090a8
Cannon Lock <clock@wisc.edu>
2024-05-08 11:16:56 -0500

    Fix redirect to index.pageContext.json

--- src/pages/maps/ingestion/+guard.ts
+++ src/pages/maps/ingestion/+guard.ts
@@ -1,15 +1,18 @@
 import { redirect, render } from "vike/abort";
+import {ingestPrefix} from "@macrostrat-web/settings";
 
 export const guard = (pageContext) => {
+
+  console.log(pageContext);
+
+
   const { user } = pageContext;
 
   if (user === undefined) {
     // Render the login page while preserving the URL. (This is novel technique
     // which we explain down below.)
     throw redirect(
-      `${
-        import.meta.env.VITE_MACROSTRAT_INGEST_API
-      }/security/login?return_url=${pageContext.urlOriginal}`
+      `${ingestPrefix}/security/login?return_url=${pageContext.urlParsed.pathname}`
     );
     /* The more traditional way, redirect the user:
     throw redirect('/login')
