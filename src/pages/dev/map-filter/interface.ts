eac4371f08d7055bbd46386035f0f1539ce74dea
Daven Quinn <code@davenquinn.com>
2024-05-13 10:26:18 -0400

    Updated filterable interface

--- src/pages/dev/map-filter/interface.ts
+++ src/pages/dev/map-filter/interface.ts
@@ -5,6 +5,7 @@ import hyper from "@macrostrat/hyper";
 import { DevMapPage } from "@macrostrat/map-interface";
 import { buildMacrostratStyle } from "@macrostrat/mapbox-styles";
 import { useStoredState } from "@macrostrat/ui-components";
+import { Select } from "@blueprintjs/select";
 import mapboxgl from "mapbox-gl";
 import { useMemo } from "react";
 import styles from "./main.module.styl";
@@ -14,6 +15,11 @@ import {
 } from "~/_utils/map-layers";
 
 export const h = hyper.styled(styles);
+
+enum Compilation {
+  Carto = "carto",
+  Maps = "maps",
+}
 
 export function VectorMapInspectorPage({
   overlayStyle = _macrostratStyle,
@@ -62,6 +68,20 @@ export function VectorMapInspectorPage({
   );
 }
 
+function CompilationSelector({ compilation, setCompilation }) {
+  return h(Select, {
+    items: Object.values(Compilation),
+    itemRenderer: (item: any, { handleClick }) => {
+      return h("div", { onClick: handleClick }, item);
+    },
+    onItemSelect: (item) => {
+      setCompilation(item);
+    },
+    filterable: false,
+    activeItem: compilation,
+  });
+}
+
 const _macrostratStyle = buildMacrostratStyle({
   tileserverDomain,
 }) as mapboxgl.Style;
@@ -79,9 +99,15 @@ function isStateValid(state) {
       return false;
     }
   }
+
+  if (typeof state.compilation != "string") {
+    return false;
+  }
+
   return true;
 }
 
 const defaultState = {
   showLineSymbols: false,
+  compilation: "carto",
 };
