c924492afd0c1ef82cfc5fc3060adcc268203115
root <root@DAVIDSLAPTOP>
2024-05-31 15:34:57 -0400

    moved to dev

--- src/pages/mainpageTest/index.ts
+++
@@ -1,6 +0,0 @@
-import h from "@macrostrat/hyper";
-
-export function Image({ src, className, width, height }) {
-    const srcWithAddedPrefix = "/img/" + src;
-    return h("img", {src: srcWithAddedPrefix, className, width, height})
-}
