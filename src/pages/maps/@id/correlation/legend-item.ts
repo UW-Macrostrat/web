670e67a7222fd233849523715309c0dd2bd55fb8
Daven Quinn <code@davenquinn.com>
2024-06-02 03:27:12 -0500

    Updated legend items

--- src/pages/maps/@id/correlation/legend-item.ts
+++ src/pages/maps/@id/correlation/legend-item.ts
@@ -26,7 +26,8 @@ export function LegendItemInformation({
           inline: false,
         }),
         h(DataField, { label: "Comments", value: legendItem.comments }),
-        h(DataField, { label: "Lithologies", value: legendItem.lith }),
+        h(DataField, { label: "Lithology", value: legendItem.lith }),
+        h(LithologyList, { lithologies: legendItem.liths }),
         h(DataField, {
           label: "Linked intervals",
           value: `${legendItem.b_interval.b_age} - ${legendItem.t_interval.t_age}`,
@@ -42,16 +43,40 @@ export function LegendItemInformation({
   ]);
 }
 
-function DataField({ label, value, inline = true }) {
-  return h("div.data-field", { className: classNames({ inline }) }, [
+function DataField({
+  label,
+  value,
+  inline = true,
+  showIfEmpty = false,
+  className,
+  children,
+  unit,
+}: {
+  label: string;
+  value?: any;
+  inline?: boolean;
+  showIfEmpty?: boolean;
+  className?: string;
+  children?: any;
+  unit?: string;
+}) {
+  let val = value ?? children;
+
+  if (!showIfEmpty && (val == null || val === "")) {
+    return null;
+  }
+
+  return h("div.data-field", { className: classNames(className, { inline }) }, [
     h("span.label", label),
-    h("span.value", value),
+    h("span.value", val),
+    h.if(unit != null)("span.unit", unit),
   ]);
 }
 
 function LithologyList({ lithologies }) {
   return h(
-    "div.lithology-list",
+    DataField,
+    { label: "Lithologies" },
     lithologies.map((lith) => {
       return h(LithologyTag, { data: lith });
     })
