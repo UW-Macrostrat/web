da6a32f1c14fad83f2d4804dec8d725724340af0
Daven Quinn <code@davenquinn.com>
2024-06-10 22:51:44 -0500

    Started some basics for correlation page

--- src/pages/correlation/+Page.ts
+++ src/pages/correlation/+Page.ts
@@ -1,15 +1,25 @@
+import { MapView } from "@macrostrat/map-interface";
 import { MapboxMapProvider } from "@macrostrat/mapbox-react";
-import { MapAreaContainer, MapView } from "@macrostrat/map-interface";
 import { FullscreenPage } from "~/layouts";
 import h from "./main.module.sass";
+import { baseMapURL, mapboxAccessToken } from "@macrostrat-web/settings";
+import { PageBreadcrumbs } from "~/renderer";
 
 export function Page() {
   return h(FullscreenPage, [
-    h("div", "Hello world"),
-    h(MapAreaContainer, h(InsetMap)),
+    h("header", [h(PageBreadcrumbs)]),
+    h("div.flex.row", [
+      h("div.correlation-diagram.grow"),
+      h("div.assistant", [h(InsetMap)]),
+    ]),
   ]);
 }
 
 function InsetMap() {
-  return h("div.column-selection-map", [h(MapView)]);
+  return h("div.column-selection-map", [
+    h(
+      MapboxMapProvider,
+      h(MapView, { style: baseMapURL, accessToken: mapboxAccessToken })
+    ),
+  ]);
 }
