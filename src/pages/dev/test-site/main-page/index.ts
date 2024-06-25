b0e1f47254c8ae849ce5fe90b0564e0df50f63c7
root <root@DAVIDSLAPTOP>
2024-06-25 14:02:48 -0400

    moved ts file

--- src/pages/dev/test-site/main-page/index.ts
+++
@@ -1,6 +0,0 @@
-import h from "@macrostrat/hyper";
-
-export function Image({ src, className, width, height }) {
-    const srcWithAddedPrefix = "https://storage.macrostrat.org/assets/web/main-page/" + src;
-    return h("img", {src: srcWithAddedPrefix, className, width, height})
-}
