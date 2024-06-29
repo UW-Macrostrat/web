91c288d42a10078ee483d7bb3368f2afa21f8d30
Daven Quinn <code@davenquinn.com>
2024-06-29 15:18:57 -0500

    Removed unused label

--- src/pages/correlation/column.ts
+++ src/pages/correlation/column.ts
@@ -97,22 +97,6 @@ const Section = (props: IColumnProps) => {
       pixelsPerMeter: pixelScale, // Actually pixels per myr
     },
     [
-      h(AgeAxis, {
-        width: 20,
-        padding: 0,
-        paddingV: 10,
-        showLabel: false,
-      }),
-      h("div.timescale-container", { style: { marginTop: `10px` } }, [
-        h(Timescale, {
-          orientation: TimescaleOrientation.VERTICAL,
-          length: height,
-          levels: timescaleLevels,
-          absoluteAgeScale: true,
-          showAgeAxis: false,
-          ageRange: range,
-        }),
-      ]),
       h(
         ColumnSVG,
         {
@@ -195,7 +179,6 @@ function Column(
     "div.column-container",
     { className },
     h("div.column", [
-      h("div.age-axis-label", "Age (Ma)"),
       h(
         "div.main-column",
         sectionGroups.map(([id, data], i) => {
@@ -225,56 +208,15 @@ function Column(
 
 function TimescaleSection(props: IColumnProps) {
   // Section with "squishy" timescale
-  const {
-    data,
-    range: _range,
-    pixelScale: _pixelScale,
-    unitComponent,
-    showLabels = true,
-    targetUnitHeight = 20,
-    width = 300,
-    columnWidth = 150,
-    unitComponentProps,
-  } = props;
+  const { range, pixelScale } = props;
 
-  const b_age = data[data.length - 1]?.b_age ?? 4200;
-  const t_age = data[0]?.t_age;
-
-  const range: [number, number] = useMemo(() => {
-    if (_range == null) {
-      return [b_age, t_age];
-    }
-    return _range;
-  }, [_range, b_age, t_age]);
-
-  const dAge = useMemo(() => range[0] - range[1], [range]);
-
-  const pixelScale = useMemo(() => {
-    if (_pixelScale != null) return _pixelScale;
-    const targetHeight = targetUnitHeight * data.length;
-    return Math.ceil(targetHeight / dAge);
-  }, [_pixelScale, targetUnitHeight, data.length, dAge]);
-
-  const height = useMemo(() => dAge * pixelScale, [dAge, pixelScale]);
-
-  /** Ensure that we can arrange units into the maximum number
-   * of columns defined by unitComponentProps, but that we don't
-   * use more than necessary.
-   */
-  const _unitComponentProps = useMemo(() => {
-    return {
-      ...unitComponentProps,
-      nColumns: Math.min(
-        Math.max(...data.map((d) => d.column)) + 1,
-        unitComponentProps?.nColumns ?? 2
-      ),
-    };
-  }, [data, unitComponentProps]);
+  const dAge = range[0] - range[1];
+  const height = dAge * pixelScale;
 
   return h(
     MacrostratColumnProvider,
     {
-      divisions: data,
+      divisions: [],
       range,
       pixelsPerMeter: pixelScale, // Actually pixels per myr
     },
@@ -295,24 +237,6 @@ function TimescaleSection(props: IColumnProps) {
           ageRange: range,
         }),
       ]),
-      h(
-        ColumnSVG,
-        {
-          innerWidth: showLabels ? width : columnWidth,
-          paddingRight: 1,
-          paddingLeft: 1,
-          paddingV: 10,
-          innerHeight: height,
-        },
-        h(CompositeUnitsColumn, {
-          width: showLabels ? width : columnWidth,
-          columnWidth,
-          gutterWidth: 5,
-          showLabels,
-          unitComponent,
-          unitComponentProps: _unitComponentProps,
-        })
-      ),
     ]
   );
 }
@@ -356,13 +280,8 @@ export function TimescaleColumn(
         sectionGroups.map(([id, data], i) => {
           const lastGroup = sectionGroups[i - 1]?.[1];
           return h([
-            h.if(unconformityLabels)(Unconformity, {
-              upperUnits: lastGroup,
-              lowerUnits: data,
-              style: { width: showLabels ? columnWidth : width },
-            }),
             h(`div.section.section-${id}`, [
-              h(Section, {
+              h(TimescaleSection, {
                 data,
                 unitComponent,
                 showLabels,
