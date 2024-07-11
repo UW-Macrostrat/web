ad3844b6a0afe98010288f7ec34c65eb2f43319b
Daven Quinn <code@davenquinn.com>
2024-07-11 11:03:32 -0500

    Simplify map bounds testing page

--- src/pages/dev/sources/+Page.client.ts
+++ src/pages/dev/sources/+Page.client.ts
@@ -1,147 +1,76 @@
 import h from "@macrostrat/hyper";
 // Import other components
 import { burwellTileDomain, mapboxAccessToken } from "@macrostrat-web/settings";
-import { DevMapPage } from "@macrostrat/map-interface";
-
-const overlayStyle = {
-  version: 8,
-  sources: {
-    rgeom: {
-      tileSize: 512,
-      type: "vector",
-      tiles: [burwellTileDomain + "/maps/bounds/{z}/{x}/{y}"],
-    },
-  },
-  layers: [
-    {
-      id: "rgeom",
-      type: "fill",
-      source: "rgeom",
-      "source-layer": "bounds",
-      paint: {
-        "fill-color": "rgba(255, 255, 255, 0.1)",
-      },
-    },
-    {
-      id: "rgeom-line",
-      type: "line",
-      source: "rgeom",
-      "source-layer": "bounds",
-      paint: {
-        "line-color": "rgba(255, 255, 255, 0.5)",
-        "line-width": 1,
-      },
-    },
-  ],
-};
-
-export function Page() {
-  // A route for each layer
-  return h(DevMapPage, {
-    title: "RGeom",
-    mapboxToken: mapboxAccessToken,
-    overlayStyle,
-  });
-}
-
 // Import other components
-import { Switch } from "@blueprintjs/core";
-import hyper from "@macrostrat/hyper";
-import { Spacer, useDarkMode, useStoredState } from "@macrostrat/ui-components";
+import { Spacer, useDarkMode } from "@macrostrat/ui-components";
 import mapboxgl from "mapbox-gl";
 import { useCallback, useState, useEffect } from "react";
-import { buildInspectorStyle, buildXRayStyle } from "./xray";
 import {
   MapMarker,
   FloatingNavbar,
   MapLoadingButton,
   MapAreaContainer,
-  PanelCard,
-} from "@macrostrat/map-interface";
-import { LocationPanel } from "../location-panel";
-import { MapView } from "../map-view";
-import styles from "./main.module.sass";
-import { TileExtentLayer } from "./tile-extent";
-import {
+  buildInspectorStyle,
+  LocationPanel,
+  MapView,
   FeaturePanel,
   FeatureSelectionHandler,
-  TileInfo,
-} from "./vector-tile-features";
-import { MapPosition } from "@macrostrat/mapbox-utils";
+} from "@macrostrat/map-interface";
 
-export enum MacrostratVectorTileset {
-  Carto = "carto",
-  CartoSlim = "carto-slim",
-  IGCPOrogens = "igcp-orogens",
-}
-
-export enum MacrostratRasterTileset {
-  Carto = "carto",
-  Emphasized = "emphasized",
-}
-
-export const h = hyper.styled(styles);
-
-export function DevMapPage({
-  title = "Map inspector",
-  headerElement = null,
-  transformRequest = null,
-  mapPosition = null,
-  mapboxToken = null,
-  overlayStyle = null,
-  children,
-  style,
-  focusedSource = null,
-  focusedSourceTitle = null,
-  projection = null,
-}: {
-  headerElement?: React.ReactElement;
-  transformRequest?: mapboxgl.TransformRequestFunction;
-  title?: string;
-  style?: mapboxgl.Style | string;
-  children?: React.ReactNode;
-  mapboxToken?: string;
-  overlayStyle?: mapboxgl.Style | string;
-  focusedSource?: string;
-  focusedSourceTitle?: string;
-  projection?: string;
-  mapPosition?: MapPosition;
-}) {
-  /* We apply a custom style to the panel container when we are interacting
-    with the search bar, so that we can block map interactions until search
-    bar focus is lost.
-    We also apply a custom style when the infodrawer is open so we can hide
-    the search bar on mobile platforms
-  */
-
+export function Page() {
   const dark = useDarkMode();
   const isEnabled = dark?.isEnabled;
 
-  if (mapboxToken != null) {
-    mapboxgl.accessToken = mapboxToken;
-  }
-
-  style ??= isEnabled
+  const baseStyle = isEnabled
     ? "mapbox://styles/mapbox/dark-v10"
     : "mapbox://styles/mapbox/light-v10";
 
   const [isOpen, setOpen] = useState(false);
 
-  const [state, setState] = useStoredState("macrostrat:dev-map-page", {
-    showTileExtent: false,
-    xRay: false,
-  });
-  const { showTileExtent, xRay } = state;
-
-  const [actualStyle, setActualStyle] = useState(style);
+  const [actualStyle, setActualStyle] = useState(null);
 
   useEffect(() => {
-    buildInspectorStyle(style, overlayStyle, {
-      mapboxToken,
+    const color = isEnabled ? 255 : 20;
+
+    const overlayStyle: mapboxgl.Style = {
+      version: 8,
+      sources: {
+        rgeom: {
+          type: "vector",
+          tiles: [burwellTileDomain + "/maps/bounds/{z}/{x}/{y}"],
+          minzoom: 1,
+          maxzoom: 9,
+        },
+      },
+      layers: [
+        {
+          id: "rgeom",
+          type: "fill",
+          source: "rgeom",
+          "source-layer": "bounds",
+          paint: {
+            "fill-color": `rgba(${color}, ${color}, ${color}, 0.1)`,
+          },
+        },
+        {
+          id: "rgeom-line",
+          type: "line",
+          source: "rgeom",
+          "source-layer": "bounds",
+          paint: {
+            "line-color": `rgba(${color}, ${color}, ${color}, 0.5)`,
+            "line-width": 1,
+          },
+        },
+      ],
+    };
+
+    buildInspectorStyle(baseStyle, overlayStyle, {
+      mapboxToken: mapboxAccessToken,
       inDarkMode: isEnabled,
-      xRay,
+      xRay: false,
     }).then(setActualStyle);
-  }, [style, xRay, mapboxToken, isEnabled, overlayStyle]);
+  }, [isEnabled]);
 
   const [inspectPosition, setInspectPosition] =
     useState<mapboxgl.LngLat | null>(null);
@@ -162,46 +91,26 @@ export function DevMapPage({
         },
         position: inspectPosition,
       },
-      [
-        h(TileInfo, {
-          feature: data?.[0] ?? null,
-          showExtent: showTileExtent,
-          setShowExtent() {
-            setState({ ...state, showTileExtent: !showTileExtent });
-          },
-        }),
-        h(FeaturePanel, { features: data, focusedSource, focusedSourceTitle }),
-      ]
+      [h(FeaturePanel, { features: data })]
     );
   }
 
-  let tile = null;
-  if (showTileExtent && data?.[0] != null) {
-    let f = data[0];
-    tile = { x: f._x, y: f._y, z: f._z };
+  if (actualStyle == null) {
+    return null;
   }
 
   return h(
     MapAreaContainer,
     {
       navbar: h(FloatingNavbar, [
-        headerElement ?? h("h2", title),
+        h("h2", "Map sources"),
         h(Spacer),
         h(MapLoadingButton, {
           active: isOpen,
           onClick: () => setOpen(!isOpen),
         }),
       ]),
-      contextPanel: h(PanelCard, [
-        h(Switch, {
-          checked: xRay,
-          label: "X-ray mode",
-          onChange() {
-            setState({ ...state, xRay: !xRay });
-          },
-        }),
-        children,
-      ]),
+      contextPanel: null,
       detailPanel: detailElement,
       contextPanelOpen: isOpen,
     },
@@ -209,10 +118,9 @@ export function DevMapPage({
       MapView,
       {
         style: actualStyle,
-        transformRequest,
-        mapPosition,
+        mapPosition: null,
         projection: { name: "globe" },
-        mapboxToken,
+        mapboxToken: mapboxAccessToken,
       },
       [
         h(FeatureSelectionHandler, {
@@ -223,7 +131,6 @@ export function DevMapPage({
           position: inspectPosition,
           setPosition: onSelectPosition,
         }),
-        h(TileExtentLayer, { tile, color: isEnabled ? "white" : "black" }),
       ]
     )
   );
