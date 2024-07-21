4d109cebf1b5744c1f4f58bb497f86b94b33c006
Daven Quinn <code@davenquinn.com>
2024-07-21 04:25:19 -0500

    Somewhat improved styles for hyperscript

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,6 +1,7 @@
 // declaration.d.ts
 
 import type { Hyper } from "@macrostrat/hyper";
+import type { Component, ReactNode, ReactElement } from "react";
 
 // Union of hyper and record
 type StyledHyper = Hyper & Record<string, string>;
@@ -25,4 +26,21 @@ declare module "*.sass" {
 declare module "*.module.sass" {
   const classes: { [key: string]: string };
   export default classes;
+}
+
+type Children = ReactNode | ReactNode[];
+
+declare module "@macrostrat/hyper" {
+  export interface Hyper {
+    // Function with one or two arguments
+    (componentOrTag: Component, children?: Children): Children;
+    // Function with three arguments, with one being props
+    <P = {}>(
+      componentOrTag: Component<P>,
+      props: P,
+      children?: ReactElement
+    ): Children;
+    // Function with one list of elements -> React fragment
+    (children?: ReactNode[]): ReactElement;
+  }
 }
