ddb0678baf0bbe83624fcec0637a561f6ea28abe
Daven Quinn <code@davenquinn.com>
2024-10-14 22:26:48 -0500

    Added preparation for multiple edit windows

--- pages/integrations/xdd/feedback/@sourceTextID/+Page.client.ts
+++ pages/integrations/xdd/feedback/@sourceTextID/+Page.client.ts
@@ -9,7 +9,9 @@ import {
   usePostgresQuery,
 } from "../../extractions/lib/data-service";
 import { FeedbackComponent } from "./lib";
-import { OverlaysProvider } from "@blueprintjs/core";
+import { Intent, NonIdealState, OverlaysProvider } from "@blueprintjs/core";
+import { ErrorBoundary, Pagination } from "@macrostrat/ui-components";
+import { useState } from "react";
 
 /**
  * Get a single text window for feedback purposes
@@ -42,24 +44,56 @@ function ExtractionIndex() {
     return h("div", "Loading...");
   }
 
+  console.log(data);
+
   return h(
-    "div.feedback-windows",
-    data.map((d) => {
-      console.log(data);
-      const window = enhanceData(d, models, entityTypes);
-      const { entities = [], paragraph_text, model } = window;
-      //h("h1", paper.citation?.title ?? "Model extractions"),
-      return h(FeedbackComponent, {
-        entities,
-        text: paragraph_text,
-        model,
-        entityTypes,
-        sourceTextID: window.source_text,
-        runID: window.model_run,
-      });
-    })
+    ErrorBoundary,
+    h(MultiFeedbackInterface, { data, models, entityTypes })
   );
 }
+
+function MultiFeedbackInterface({ data, models, entityTypes }) {
+  const [ix, setIX] = useState(0);
+  const currentData = data[ix];
+  const count = data.length;
+
+  return h("div.feedback", [
+    h.if(data.length > 1)([
+      h(NonIdealState, {
+        icon: "warning-sign",
+        title: "Multiple model runs for feedback",
+        description: `Showing entities from ${
+          ix + 1
+        } of ${count} model runs. Merging several runs is not yet supported.`,
+      }),
+      h(Pagination, {
+        count,
+        page: ix,
+        setPage: setIX,
+        nextDisabled: ix >= count - 1,
+      }),
+    ]),
+    h(FeedbackInterface, {
+      data: currentData,
+      models,
+      entityTypes,
+    }),
+  ]);
+}
+
+function FeedbackInterface({ data, models, entityTypes }) {
+  const window = enhanceData(data, models, entityTypes);
+  const { entities = [], paragraph_text, model } = window;
+  return h(FeedbackComponent, {
+    entities,
+    text: paragraph_text,
+    model,
+    entityTypes,
+    sourceTextID: window.source_text,
+    runID: window.model_run,
+  });
+}
+
 
 // function FeedbackDevTool() {
 //   const entities = useStore((state) => state.entities);
