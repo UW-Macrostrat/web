933bdee68fb19b716a69a5b578dd3283bf105632
Daven Quinn <code@davenquinn.com>
2024-11-26 09:40:00 -0600

    Removed a ton of styles

--- pages/dev/map/rockd-strabospot/details-panel.ts
+++ pages/dev/map/rockd-strabospot/details-panel.ts
@@ -1,81 +1,15 @@
-import { Spinner, Switch } from "@blueprintjs/core";
-import { useMapRef, useMapStatus } from "@macrostrat/mapbox-react";
-import mapboxgl from "mapbox-gl";
+import { Spinner } from "@blueprintjs/core";
+import { useMapRef } from "@macrostrat/mapbox-react";
 import hyper from "@macrostrat/hyper";
 import styles from "./main.module.sass";
 import { useEffect, useState } from "react";
-import { JSONView, usePrevious } from "@macrostrat/ui-components";
 import { group } from "d3-array";
-import { ExpansionPanel } from "@macrostrat/map-interface";
+import { ExpansionPanel, FeatureRecord } from "@macrostrat/map-interface";
 
 const h = hyper.styled(styles);
 
-export function FeatureProperties({ data, ...rest }) {
-  return h("div.feature-properties", [
-    h(JSONView, {
-      data,
-      hideRoot: true,
-      ...rest,
-    }),
-  ]);
-}
-
-export function FeatureRecord({ feature }) {
-  const props = feature.properties;
-  return h("div.feature-record", [
-    h.if(Object.keys(props).length > 0)(FeatureProperties, { data: props }),
-  ]);
-}
-
-export function FeatureSelectionHandler({
-  selectedLocation,
-  setFeatures,
-  radius = 2,
-}: {
-  selectedLocation: mapboxgl.LngLat;
-  setFeatures: (features: mapboxgl.MapboxGeoJSONFeature[]) => void;
-  radius?: number;
-}) {
-  const mapRef = useMapRef();
-  const { isLoading } = useMapStatus();
-  const prevLocation = usePrevious(selectedLocation);
-
-  useEffect(() => {
-    const map = mapRef?.current;
-    if (map == null) return;
-    if (selectedLocation == null) {
-      setFeatures(null);
-      return;
-    }
-
-    // Don't update if the location hasn't changed
-    if (selectedLocation == prevLocation) return;
-
-    const r = radius;
-    const pt = map.project(selectedLocation);
-
-    const bbox: [mapboxgl.PointLike, mapboxgl.PointLike] = [
-      [pt.x - r, pt.y - r],
-      [pt.x + r, pt.y + r],
-    ];
-    const features = map.queryRenderedFeatures(bbox);
-    setFeatures(features);
-  }, [mapRef.current, prevLocation?.current, selectedLocation, isLoading]);
-
-  return null;
-}
-
 function FeatureHeader({ feature }) {
-  return h("div.feature-header", [
-    h("h3", [
-      h(KeyValue, { label: "Source", value: feature.source }),
-      h(KeyValue, { label: "Source layer", value: feature.sourceLayer }),
-    ]),
-  ]);
-}
-
-function KeyValue({ label, value }) {
-  return h("span.key-value", [h("span.key", label), h("code.value", value)]);
+  return h("div.feature-header", [h("h3", [h("code", feature.source)])]);
 }
 
 function LoadingAwareFeatureSet({ features, sourceID }) {
@@ -106,48 +40,7 @@ function LoadingAwareFeatureSet({ features, sourceID }) {
   return h(Features, { features: sourceFeatures });
 }
 
-export function TileInfo({ feature, showExtent, setShowExtent }) {
-  if (feature == null) return null;
-  const size = feature._vectorTileFeature._pbf.length;
-  return h("div.tile-info", [
-    h("h3", "Tile"),
-    h("div.tile-index", [
-      h(KeyValue, { label: "x", value: feature._x }),
-      h(KeyValue, { label: "y", value: feature._y }),
-      h(KeyValue, { label: "z", value: feature._z }),
-    ]),
-    h("div.spacer"),
-    h(KeyValue, { label: "Size", value: formatSize(size) }),
-    h(Switch, {
-      label: "Show extent",
-      alignIndicator: "right",
-      checked: showExtent,
-      onChange() {
-        setShowExtent(!showExtent);
-      },
-    }),
-  ]);
-}
-
-function formatSize(size: number) {
-  if (size > 1000000)
-    return h(UnitNumber, { value: size / 1000000, unit: "Mb" });
-  if (size > 1000) return h(UnitNumber, { value: size / 1000, unit: "Kb" });
-  return `${size} bytes`;
-}
-
-function UnitNumber({ value, unit, precision = 1 }) {
-  return h("span.unit-number", [
-    h("span.number", value.toFixed(precision)),
-    h("span.unit", unit),
-  ]);
-}
-
-export function FeaturePanel({
-  features,
-  focusedSource = null,
-  focusedSourceTitle = null,
-}) {
+export function FeaturePanel({ features, focusedSource = null }) {
   if (features == null) return null;
 
   let focusedSourcePanel = null;
