f077822827a31160b110de1adcc0d72650265e4a
Daven Quinn <code@davenquinn.com>
2025-02-16 04:03:32 -0600

    Fix page scrolling

--- pages/+config.ts
+++ pages/+config.ts
@@ -5,6 +5,7 @@ import type { Config } from "vike/types";
 // Default config (can be overridden by pages)
 export default {
   title: "Macrostrat",
+  pageStyle: "content",
   description:
     "A platform for geological data exploration, integration, and analysis.",
   extends: vikeReact,
