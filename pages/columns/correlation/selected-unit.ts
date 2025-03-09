ba13ab0525551d951e039abe17353b622de45344
Daven Quinn <code@davenquinn.com>
2025-03-09 16:30:21 -0600

    Starting point to expand units laterally

--- pages/columns/correlation/selected-unit.ts
+++ pages/columns/correlation/selected-unit.ts
@@ -1,11 +1,8 @@
 import { ForeignObject, useColumn } from "@macrostrat/column-components";
 import { UnitDetailsPanel, useSelectedUnit } from "@macrostrat/column-views";
-import { DOMElement, useEffect, useRef } from "react";
+import { DOMElement } from "react";
 import { UnitDetailsPopover } from "~/components/unit-details";
 import h from "@macrostrat/hyper";
-// import styles from "./column.module.scss";
-//
-// const h = hyper.styled(styles);
 
 export function SelectedUnitPopoverContainer({ width, height, padding = 0 }) {
   const extraWidth = padding * 2;
