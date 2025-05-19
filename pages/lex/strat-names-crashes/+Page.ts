cd3990392b2f3bb10cc11ff1c5399f468278b497
davidsklar99 <davidsklar99@outlook.com>
2025-05-19 10:16:03 -0500

    new strat page

---
+++ pages/lex/strat-names-crashes/+Page.ts
@@ -0,0 +1,158 @@
+import hyper from "@macrostrat/hyper";
+import { ContentPage } from "~/layouts";
+import {
+  PageHeader,
+  Link,
+  AttributedLithTag,
+  PageBreadcrumbs,
+} from "~/components";
+import {
+  InputGroup,
+  Spinner,
+  Button,
+  Collapse,
+  Card,
+  Switch,
+} from "@blueprintjs/core";
+import { InfiniteScroll } from "@macrostrat/ui-components";
+import { useReducer, useEffect, useState } from "react";
+import {
+  FilterState,
+  useDebouncedStratNames,
+  defaultFilterState,
+} from "./data-service";
+import { setQueryString } from "@macrostrat/ui-components";
+import styles from "./main.module.sass";
+import { usePageProps } from "~/renderer/usePageProps";
+
+const h = hyper.styled(styles);
+
+function filterReducer(state: FilterState, action): FilterState {
+  switch (action.type) {
+    case "set-search-string":
+      if (action.value === "") {
+        action.value = null;
+      }
+      return { ...state, match: action.value };
+    case "toggle-candidate-liths":
+      return { ...state, candidates: state.candidates == null ? true : null };
+  }
+}
+
+function compareQueryParams(a, b) {
+  let newParams = {};
+  for (let key in a) {
+    if (a[key] !== b[key]) {
+      newParams[key] = a[key];
+    }
+  }
+  if (Object.keys(newParams).length === 0) {
+    return null;
+  }
+
+  return newParams;
+}
+
+export function Page() {
+  const { data, filters: startingFilters } = usePageProps();
+  const [showSettings, setShowSettings] = useState(false);
+  const [state, dispatch] = useReducer(filterReducer, startingFilters);
+
+  console.log("data", data)
+
+  useEffect(() => {
+    let s1 = state;
+    if (s1.match === "") {
+      delete s1.match;
+    }
+    const q = compareQueryParams(s1, startingFilters);
+    if (q != null) {
+      setQueryString(s1);
+    }
+  }, [state]);
+
+  return h(ContentPage, [
+    h("div.page-header.stick-to-top", [
+      h("div.flex.row.align-center", [
+        h(PageBreadcrumbs, { showLogo: true }),
+        h("div.spacer"),
+        h(FilterControl, {
+          searchString: state.match,
+          setSearchString(value) {
+            dispatch({ type: "set-search-string", value });
+          },
+        }),
+        /* TODO
+        h(Button, {
+          icon: "settings",
+          minimal: true,
+          active: showSettings,
+          onClick() {
+            setShowSettings(!showSettings);
+          },
+        }),
+         */
+      ]),
+      //h(SettingsPanel, { isOpen: showSettings, state, dispatch }),
+    ]),
+    h(StratNamesView, { initialData: data, filters: state }),
+    h("div.background-placeholder"),
+  ]);
+}
+
+function SettingsPanel({ isOpen = false, state, dispatch }) {
+  return h(Collapse, { isOpen }, [
+    h(Card, { elevation: 0 }, [
+      h(Switch, {
+        label: "Limit to units with candidate lithologies",
+        checked: state.candidates != null,
+        onChange() {
+          dispatch({ type: "toggle-candidate-liths" });
+        },
+      }),
+    ]),
+  ]);
+}
+
+function FilterControl({ searchString, setSearchString }) {
+  return h("div.filter-control", [
+    h(InputGroup, {
+      leftIcon: "filter",
+      placeholder: "Filter",
+      value: searchString,
+      onChange(e) {
+        setSearchString(e.target.value);
+      },
+    }),
+  ]);
+}
+
+function StratNamesView({ initialData, filters }) {
+  const [{ data, error, isLoading, hasMore }, loadNextPage] =
+    useDebouncedStratNames(filters, { perPage: 20, delay: 300 }, initialData);
+
+  return h(
+    InfiniteScroll,
+    {
+      hasMore,
+      loadMore() {
+        loadNextPage();
+      },
+    },
+    [
+      h(StratNamesList, { data }),
+      h.if(isLoading)(Spinner),
+      h.if(!hasMore && !isLoading)("p", "No more data"),
+    ]
+  );
+}
+
+function StratNamesList({ data }) {
+  if (data == null) {
+    return null;
+  }
+
+  return h("div.strat-names-list", [
+    data.map((d) => h(StratNameItem, { data: d, key: d.id })),
+  ]);
+}
--- pages/lex/strat-names/+Page.ts
+++ pages/lex/strat-names/+Page.ts
@@ -1,158 +1,64 @@
-import hyper from "@macrostrat/hyper";
+import h from "./main.module.scss";
+import { useAPIResult } from "@macrostrat/ui-components";
+import { SETTINGS } from "@macrostrat-web/settings";
+import { PageHeader, Link, AssistantLinks, DevLinkButton } from "~/components";
+import { Card, Icon, Popover, RangeSlider } from "@blueprintjs/core";
+import { useState } from "react";
 import { ContentPage } from "~/layouts";
-import {
-  PageHeader,
-  Link,
-  AttributedLithTag,
-  PageBreadcrumbs,
-} from "~/components";
-import {
-  InputGroup,
-  Spinner,
-  Button,
-  Collapse,
-  Card,
-  Switch,
-} from "@blueprintjs/core";
-import { InfiniteScroll } from "@macrostrat/ui-components";
-import { useReducer, useEffect, useState } from "react";
-import {
-  FilterState,
-  useDebouncedStratNames,
-  defaultFilterState,
-} from "./data-service";
-import { setQueryString } from "@macrostrat/ui-components";
-import styles from "./main.module.sass";
-import { usePageProps } from "~/renderer/usePageProps";
+import { usePageContext } from 'vike-react/usePageContext';
 
-const h = hyper.styled(styles);
-
-function filterReducer(state: FilterState, action): FilterState {
-  switch (action.type) {
-    case "set-search-string":
-      if (action.value === "") {
-        action.value = null;
-      }
-      return { ...state, match: action.value };
-    case "toggle-candidate-liths":
-      return { ...state, candidates: state.candidates == null ? true : null };
-  }
-}
-
-function compareQueryParams(a, b) {
-  let newParams = {};
-  for (let key in a) {
-    if (a[key] !== b[key]) {
-      newParams[key] = a[key];
-    }
-  }
-  if (Object.keys(newParams).length === 0) {
-    return null;
-  }
-
-  return newParams;
-}
 
 export function Page() {
-  const { data, filters: startingFilters } = usePageProps();
-  const [showSettings, setShowSettings] = useState(false);
-  const [state, dispatch] = useReducer(filterReducer, startingFilters);
+    const res = useAPIResult(SETTINGS.apiV2Prefix + "/defs/strat_names?all")?.success.data;
 
-  console.log("data", data)
+    if (res == null) return h("div", "Loading...");
 
-  useEffect(() => {
-    let s1 = state;
-    if (s1.match === "") {
-      delete s1.match;
-    }
-    const q = compareQueryParams(s1, startingFilters);
-    if (q != null) {
-      setQueryString(s1);
-    }
-  }, [state]);
+    const grouped = res.reduce((acc, item) => {
+        const { rank, subgp } = item;
 
-  return h(ContentPage, [
-    h("div.page-header.stick-to-top", [
-      h("div.flex.row.align-center", [
-        h(PageBreadcrumbs, { showLogo: true }),
-        h("div.spacer"),
-        h(FilterControl, {
-          searchString: state.match,
-          setSearchString(value) {
-            dispatch({ type: "set-search-string", value });
-          },
-        }),
-        /* TODO
-        h(Button, {
-          icon: "settings",
-          minimal: true,
-          active: showSettings,
-          onClick() {
-            setShowSettings(!showSettings);
-          },
-        }),
-         */
-      ]),
-      //h(SettingsPanel, { isOpen: showSettings, state, dispatch }),
-    ]),
-    h(StratNamesView, { initialData: data, filters: state }),
-    h("div.background-placeholder"),
-  ]);
+        if (!acc[rank]) {
+            acc[rank] = {};
+        }
+
+        if (!acc[rank][subgp]) {
+            acc[rank][subgp] = [];
+        }
+
+        acc[rank][subgp].push(item);
+
+        return acc;
+    }, {});
+
+
+    console.log(grouped);
+
+    return h(ContentPage,  [
+        h(PageHeader, { title: "Strat Names" }),
+        h('div.strat-list',
+        Object.entries(grouped).map(([rank, subgps]) =>
+            h('div.strat-rank-group', [
+            h('h2', `Rank: ${rank}`),
+            ...Object.entries(subgps).map(([subgp, items]) =>
+                h('div.strat-subgroup', [
+                h('h3', `Subgroup: ${subgp.toUpperCase()}`),
+                h('div.strat-items',
+                    
+                    items.map(data =>
+                        h(StratItem, { data })
+                    )
+                    
+                )
+                ])
+            )
+            ])
+        )
+    )
+        ])
 }
 
-function SettingsPanel({ isOpen = false, state, dispatch }) {
-  return h(Collapse, { isOpen }, [
-    h(Card, { elevation: 0 }, [
-      h(Switch, {
-        label: "Limit to units with candidate lithologies",
-        checked: state.candidates != null,
-        onChange() {
-          dispatch({ type: "toggle-candidate-liths" });
-        },
-      }),
-    ]),
-  ]);
+function StratItem({ data }) {
+    const { strat_name, strat_name_id } = data;
+
+    return h('a.strat-name', { href: "/strat-names/" + strat_name_id}, strat_name)
 }
 
-function FilterControl({ searchString, setSearchString }) {
-  return h("div.filter-control", [
-    h(InputGroup, {
-      leftIcon: "filter",
-      placeholder: "Filter",
-      value: searchString,
-      onChange(e) {
-        setSearchString(e.target.value);
-      },
-    }),
-  ]);
-}
-
-function StratNamesView({ initialData, filters }) {
-  const [{ data, error, isLoading, hasMore }, loadNextPage] =
-    useDebouncedStratNames(filters, { perPage: 20, delay: 300 }, initialData);
-
-  return h(
-    InfiniteScroll,
-    {
-      hasMore,
-      loadMore() {
-        loadNextPage();
-      },
-    },
-    [
-      h(StratNamesList, { data }),
-      h.if(isLoading)(Spinner),
-      h.if(!hasMore && !isLoading)("p", "No more data"),
-    ]
-  );
-}
-
-function StratNamesList({ data }) {
-  if (data == null) {
-    return null;
-  }
-
-  return h("div.strat-names-list", [
-    data.map((d) => h(StratNameItem, { data: d, key: d.id })),
-  ]);
-}
