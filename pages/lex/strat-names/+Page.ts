34e1442e6ab06b4d781925c3b7515cc3a6b01164
davidsklar99 <davidsklar99@outlook.com>
2025-05-19 11:00:14 -0500

    filter using api instead of frontend

--- pages/lex/strat-names/+Page.ts
+++ pages/lex/strat-names/+Page.ts
@@ -1,7 +1,7 @@
 import h from "./main.module.scss";
 import { useAPIResult } from "@macrostrat/ui-components";
 import { SETTINGS } from "@macrostrat-web/settings";
-import { PageHeader, Link, AssistantLinks, DevLinkButton, LinkCard } from "~/components";
+import { PageHeader, Link, AssistantLinks, DevLinkButton, LinkCard, PageBreadcrumbs } from "~/components";
 import { Card, Icon, Popover, RangeSlider } from "@blueprintjs/core";
 import { useState } from "react";
 import { ContentPage } from "~/layouts";
@@ -9,8 +9,8 @@ import { usePageContext } from 'vike-react/usePageContext';
 
 
 export function Page() {
-    const res = useAPIResult(SETTINGS.apiV2Prefix + "/defs/strat_names?all")?.success.data;
     const [input, setInput] = useState("");
+    const res = useAPIResult(SETTINGS.apiV2Prefix + "/defs/strat_names?strat_name_like=" + input)?.success.data;
 
     if (res == null) return h("div", "Loading...");
 
@@ -34,34 +34,8 @@ export function Page() {
         setInput(event.target.value.toLowerCase());
     }
 
-    const filtered = Object.entries(grouped).reduce((acc, [rank, gps]) => {
-        const filteredGps = Object.entries(gps).reduce((acc, [gp, subgps]) => {
-            const filteredSubgps = Object.entries(subgps).reduce((acc, [subgp, items]) => {
-                const filteredItems = items.filter((item) => {
-                    const name = item.strat_name?.toLowerCase() || "";
-                    const gp = item.gp?.toLowerCase() || "";
-                    const subgp = item.subgp?.toLowerCase() || "";
-                    const rank = item.rank?.toLowerCase() || "";
-                    return name.includes(input) || gp.includes(input) || subgp.includes(input) || rank.includes(input);
-                });
-                if (filteredItems.length > 0) {
-                    acc[subgp] = filteredItems;
-                }
-                return acc;
-            }, {});
-            if (Object.keys(filteredSubgps).length > 0) {
-                acc[gp] = filteredSubgps;
-            }
-            return acc;
-        }, {});
-        if (Object.keys(filteredGps).length > 0) {  
-            acc[rank] = filteredGps;
-        }
-        return acc;
-    }, {});
-
     return h(ContentPage, [
-        h(PageHeader, { title: "Strat Names" }),
+        h(PageBreadcrumbs, { title: "Strat Names" }),
         h(Card, [
             h("h2", 'Filter'),
             h('div.search-bar', [
@@ -74,7 +48,7 @@ export function Page() {
             ])
         ]),
         h('div.strat-list',
-            Object.entries(filtered).map(([rank, gps]) =>
+            Object.entries(grouped).map(([rank, gps]) =>
             h('div.strat-rank-group', [
                 h('h2', `Rank: ${rank}`),
 
