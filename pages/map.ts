ef62ce88e16be071f7706475af0ce7cc19d81300
davidsklar99 <davidsklar99@outlook.com>
2025-05-20 12:39:27 -0500

    map styling

--- pages/map.ts
+++ pages/map.ts
@@ -1,7 +1,10 @@
-import h from "@macrostrat/hyper";
+import { hyperStyled } from "@macrostrat/hyper";
 import { ColumnNavigationMap } from "@macrostrat/column-views";
 import { mapboxAccessToken } from "@macrostrat-web/settings";
 import { ErrorBoundary } from "@macrostrat/ui-components";
+import styles from "./layout.module.sass";
+
+const h = hyperStyled(styles)
 
 export function ColumnMap({
   projectID,
