e4cbafd20d83ed6aed2b518dda9c97e736fcaf09
davidsklar99 <davidsklar99@outlook.com>
2025-05-20 10:08:48 -0500

    test map works

---
+++ pages/dev/test/+Page.client.ts
@@ -0,0 +1,37 @@
+import {
+  ColoredUnitComponent,
+  MacrostratDataProvider,
+  Column,
+} from "@macrostrat/column-views";
+import { hyperStyled } from "@macrostrat/hyper";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { apiV2Prefix } from "@macrostrat-web/settings";
+import { PatternProvider } from "~/_providers";
+import { navigate } from "vike/client/router";
+import { PageBreadcrumbs } from "~/components";
+import { onDemand } from "~/_utils";
+import styles from "./index.module.sass";
+const h = hyperStyled(styles);
+
+
+const ColumnMap = onDemand(() => import("./map").then((mod) => mod.ColumnMap));
+
+function dsf(props) {
+  return h(
+    MacrostratDataProvider,
+    { baseURL: apiV2Prefix },
+    h(PatternProvider, h(ColumnPageInner, props))
+  );
+}
+
+export function Page() {
+  return h("div.page-container", [
+          h(ColumnMap, {
+            className: "column-map",
+            inProcess: true,
+            projectID: null,
+            selectedColumn: 1,
+            onSelectColumn: () => {},
+          }),
+        ])
+}
