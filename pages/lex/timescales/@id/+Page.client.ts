f116731cf2eda4aa04badda883982350120ce51e
davidsklar99 <davidsklar99@outlook.com>
2025-05-26 17:12:03 -0500

    on click works

--- pages/lex/timescales/@id/+Page.client.ts
+++ pages/lex/timescales/@id/+Page.client.ts
@@ -8,6 +8,7 @@ import { usePageContext } from 'vike-react/usePageContext';
 import { Timescale } from "@macrostrat/timescale";
 import { titleCase } from "../../index";
 import { useState, useEffect } from "react";
+import { CarbonIsotopesColumn } from "@macrostrat/column-views";
 
 
 export function Page() {
@@ -15,6 +16,28 @@ export function Page() {
     const id = parseInt(pageContext.urlParsed.pathname.split("/")[3]);
     const res = useAPIResult(SETTINGS.apiV2Prefix + "/defs/timescales?all")?.success.data;
     const [clickedInterval, setClickedInterval] = useState(null);
+
+    console.log('clicked', clickedInterval)
+
+    useEffect(() => {
+      if (!clickedInterval) return;
+
+      const fetchInterval = async () => {
+        try {
+          const res = await fetch(`https://macrostrat.org/api/defs/intervals?name=${clickedInterval}`);
+          const data = await res.json();
+          const clickedData = data?.success?.data?.[0];
+          if (clickedData) {
+            const url = "/lex/intervals/" + clickedData.int_id;
+            window.open(url, "_blank");
+          }
+        } catch (error) {
+          console.error("Error fetching interval data:", error);
+        }
+      };
+
+      fetchInterval();
+    }, [clickedInterval]);
 
     if (res == null) return h("div", "Loading...");
 
@@ -26,8 +49,6 @@ export function Page() {
     const { min_age, max_age, timescale } = timeRes;
 
     const handleClick = (timescale) => {
-        console.log("Clicked timescale:", timescale);
-        /*
         const parent = timescale.target.parentElement;
         let selected;
 
@@ -42,15 +63,20 @@ export function Page() {
         }
 
         setClickedInterval(selected);
-        */
     }
 
     return h(ContentPage, [
         h(PageBreadcrumbs, { title: "#" + id }),
         h('div.timescale-content', [
-            h('h1', titleCase(timescale)),
-            h('h3', max_age + " - " +  min_age + " Ma"),
-            h('div.timescale', h(Timescale, { levels: [0,5], ageRange: [min_age, max_age], orientation: "vertical"})),
+          h('h1', titleCase(timescale)),
+          h('h3', max_age + " - " +  min_age + " Ma"),
+          h(Timescale, { 
+              levels: [0,5], 
+              ageRange: [min_age, max_age], 
+              orientation: "vertical",
+              absoluteAgeScale: true,  
+              onClick: handleClick, 
+            })
         ])
     ]);
 }
