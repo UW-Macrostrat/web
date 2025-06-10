2656a0892a805c86da2abc416b1fd4e18ae7cb53
davidsklar99 <davidsklar99@outlook.com>
2025-06-10 13:17:24 -0500

    add better description

--- pages/lex/strat-name-concepts/+Page.client.ts
+++ pages/lex/strat-name-concepts/+Page.client.ts
@@ -5,14 +5,20 @@ import { ContentPage } from "~/layouts";
 export function Page() {
   return h(ContentPage, [
     h(PageBreadcrumbs),
-    h(
-      "h2",
-      "Stratigraphic names are the names of rock units, organized hierarchically."
-    ),
-    h(
-      "h2",
-      "Stratigraphic concepts capture relationships between differently-named rock units."
-    ),
+    h("div.names", [
+      h("h2", "Stratigraphic Names"),
+      h("p", [
+        h("em", "Stratigraphic names"),
+        " are the names of rock units, organized hierarchically."
+      ]),
+    ]),
+    h("div.concepts", [
+      h("h2", "Stratigraphic Concepts"),
+      h("p", [
+        h("em", "Stratigraphic concepts"),
+        " capture the complexity associated with the usage of stratigraphic names over a long period of time by many workers. A concept establishes a set of stratigraphic names that describe the same rock unit."
+      ]),
+    ]),
     h(
       "h2",
       h(
