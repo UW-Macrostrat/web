f6f194f26d6adf106ad80949102e16306f62344b
davidsklar99 <davidsklar99@outlook.com>
2025-06-23 09:06:22 -0500

    Load more for fossils, maps, and units

--- src/components/lex/index.ts
+++ src/components/lex/index.ts
@@ -885,23 +885,40 @@ function ChartLegend(data, route, activeIndex, setActiveIndex, index) {
 }
 
 export function Units({ unitsData }) {
-  return h.if(unitsData.length > 0)('div.units-container', [
-    h(ExpansionPanelContainer, { title: "Units" }, 
-      h('div.units-list', 
-        unitsData.map(unit => 
-          h('a.unit-item', { href: "/columns/" + unit.col_id + "#unit=" + unit.unit_id }, unit.unit_name + " (#" + unit.unit_id + ")")
-        )
+  const ITEMS_PER_PAGE = 20;
+  const [visibleCount, setVisibleCount] = useState(ITEMS_PER_PAGE);
+  const data = useMemo(() => {
+    return unitsData.slice(0, visibleCount);
+  }, [unitsData, visibleCount]);
+
+  const visibleItems = data.map(item =>
+    h('a.unit-item', { href: "/columns/" + item.col_id + "#unit=" + item.unit_id }, item.unit_name + " (#" + item.unit_id + ")")
+  );
+
+  const handleLoadMore = () => {
+    setVisibleCount(prev => Math.min(prev + ITEMS_PER_PAGE, unitsData.length));
+  };
+
+  const showLoadMore = visibleCount < unitsData.length;
+
+  return h('div.units-container', [
+    h(ExpansionPanel, { title: "Units", className: "units-panel" }, [
+      h('div.units-list', [
+        ...visibleItems,
+      ]),
+      h.if(showLoadMore)('div.load-more-wrapper',
+        h('button.load-more-btn', { onClick: handleLoadMore }, 'Load More')
       )
-    )
-  ])
+    ])
+  ]);
 }
 
-
-export 
-function Maps({ mapsData }) {
-  const ITEMS_PER_PAGE = 10;
+export function Maps({ mapsData }) {
+  const ITEMS_PER_PAGE = 20;
   const [visibleCount, setVisibleCount] = useState(ITEMS_PER_PAGE);
-  const data = mapsData.slice(0, visibleCount);
+  const data = useMemo(() => {
+    return mapsData.slice(0, visibleCount);
+  }, [mapsData, visibleCount]);
 
   const visibleItems = data.map(item =>
     h('a.maps-item', { key: item.map_unit_name, href: "/maps/" + item.source_id + "?legend=" + item.legend_id },
@@ -916,24 +933,42 @@ function Maps({ mapsData }) {
   const showLoadMore = visibleCount < mapsData.length;
 
   return h('div.maps-container', [
-    h(ExpansionPanel, { title: "Maps" },
+    h(ExpansionPanel, { title: "Maps", className: "maps-panel" }, [
       h('div.maps-list', [
         ...visibleItems,
-        h.if(showLoadMore)('button.load-more-btn', { onClick: handleLoadMore }, 'Load More')
-      ])
-    )
+      ]),
+      h.if(showLoadMore)('div.load-more-wrapper',
+        h('button.load-more-btn', { onClick: handleLoadMore }, 'Load More')
+      )
+    ])
   ]);
 }
 
-
 export function Fossils({ fossilsData }) {
-  return h.if(fossilsData.length > 0)('div.fossils-container', [
-    h(ExpansionPanelContainer, { title: "Fossils" }, 
-      h('div.fossils-list', 
-        fossilsData.map(fossil => 
-          h('a.fossil-item', { href: `https://paleobiodb.org/classic/displayCollResults?collection_no=col:${fossil.cltn_id}` }, fossil.cltn_name + " (#" + fossil.cltn_id + ")")
-        )
+  const ITEMS_PER_PAGE = 20;
+  const [visibleCount, setVisibleCount] = useState(ITEMS_PER_PAGE);
+  const data = useMemo(() => {
+    return fossilsData.slice(0, visibleCount);
+  }, [fossilsData, visibleCount]);
+
+  const visibleItems = data.map(item =>
+    h('a.fossil-item', { href: `https://paleobiodb.org/classic/displayCollResults?collection_no=col:${item.cltn_id}` }, item.cltn_name + " (#" + item.cltn_id + ")")
+  );
+
+  const handleLoadMore = () => {
+    setVisibleCount(prev => Math.min(prev + ITEMS_PER_PAGE, fossilsData.length));
+  };
+
+  const showLoadMore = visibleCount < fossilsData.length;
+
+  return h('div.fossils-container', [
+    h(ExpansionPanel, { title: "Fossils", className: "fossils-panel" }, [
+      h('div.fossils-list', [
+        ...visibleItems,
+      ]),
+      h.if(showLoadMore)('div.load-more-wrapper',
+        h('button.load-more-btn', { onClick: handleLoadMore }, 'Load More')
       )
-    )
-  ])
+    ])
+  ]);
 }
