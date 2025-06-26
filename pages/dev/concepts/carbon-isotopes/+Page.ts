7f87a285726dd75f2f2a3a8644ebb37e52d39f21
davidsklar99 <davidsklar99@outlook.com>
2025-06-26 13:29:31 -0500

    Fix build issue

--- pages/dev/concepts/carbon-isotopes/+Page.ts
+++
@@ -1,7 +0,0 @@
-import h from "@macrostrat/hyper";
-import { CarbonIsotopesApp } from "@macrostrat/column-views";
-import { PatternProvider } from "~/_providers";
-
-export function Page() {
-  return h(PatternProvider, h(CarbonIsotopesApp));
-}
