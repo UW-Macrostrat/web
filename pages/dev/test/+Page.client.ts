b9875ce1088c044186e434d23fc08d7894a31012
davidsklar99 <davidsklar99@outlook.com>
2025-06-26 12:40:47 -0500

    Map in lex page, onSelectColumn doesn't work

--- pages/dev/test/+Page.client.ts
+++ pages/dev/test/+Page.client.ts
@@ -1,10 +1,23 @@
 import h from "@macrostrat/hyper";
 import { ColumnNavigationMap } from "@macrostrat/column-views";
 import { mapboxAccessToken } from "@macrostrat-web/settings";
+import { useAPIResult } from "@macrostrat/ui-components";
 
 export function Page() {
-  return h(ColumnNavigationMap, { 
-    style: { height: "100vh" },
-    accessToken: mapboxAccessToken 
-  });
+    let columns = useAPIResult('https://dev.macrostrat.org/api/columns', {
+        col_id: 2,
+        format: 'geojson'
+    })?.success.data.features;
+
+    if(!columns) {
+        return h("div", { className: "error" }, "Error loading columns data.");
+    }
+
+    console.log("ColumnsMapInner", columns);
+
+    return h(ColumnNavigationMap, { 
+        style: { height: "100vh" },
+        columns,
+        accessToken: mapboxAccessToken 
+    });
 }
--- src/components/lex/map.client.ts
+++ src/components/lex/map.client.ts
@@ -5,7 +5,6 @@ import {
 import h from "@macrostrat/hyper";
 import { mapboxAccessToken } from "@macrostrat-web/settings";
 import { ErrorBoundary } from "@macrostrat/ui-components";
-import { ColumnsMap } from "~/columns-map/index.client";
 import { ExpansionPanel } from "@macrostrat/map-interface";
 
 
@@ -20,26 +19,23 @@ export function ExpansionPanelContainer(props) {
 
 function ColumnsMapInner({
   columnIDs = null,
-  projectID = null,
   className,
   columns = null,
 }) {
-  const columnData = useMacrostratColumns(projectID, projectID != null);
+  columns = columns.features
 
-  let _columns = columns?.features ?? columnData;
-
-  // Filter columns on the client side
-  if (columnIDs != null) {
-    _columns = _columns.filter((feature) =>
-      columnIDs.includes(feature.properties.col_id)
-    );
-  }
-
+  console.log("ColumnsMapInner", columns);
+  
   return h(
     "div",
     { className },
-    h(ColumnsMap, {
-      columns: { type: "FeatureCollection", features: _columns },
+    h(ColumnNavigationMap, {
+      columns,
+      accessToken: mapboxAccessToken,
+      style: {height: "100%"},
+      onSelectColumn: (e, data) => {
+        console.log("Selected column:", data.properties.col_id);
+      }
     })
   );
 }
