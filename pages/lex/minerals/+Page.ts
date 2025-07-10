b4b9f290c3f9435947bb12f622bb9fb3d4aa490a
davidsklar99 <davidsklar99@outlook.com>
2025-07-10 12:34:35 -0500

    Mineral page uses new component

--- pages/lex/minerals/+Page.ts
+++ pages/lex/minerals/+Page.ts
@@ -1,10 +1,8 @@
 import h from "./main.module.sass";
-import { InfiniteScrollView } from "@macrostrat/ui-components";
+import { PostgRESTInfiniteScrollView } from "@macrostrat/ui-components";
 import { apiDomain } from "@macrostrat-web/settings";
 import { StickyHeader, LinkCard, PageBreadcrumbs } from "~/components";
-import { useState } from "react";
 import { ContentPage } from "~/layouts";
-import { SearchBar } from "~/components/general";
 import { useData } from "vike-react/useData";
 
 const PAGE_SIZE = 20;
@@ -12,37 +10,20 @@ const PAGE_SIZE = 20;
 export function Page() {
   const { res } = useData();
 
-  const [input, setInput] = useState("");
-
-  const handleChange = (event) => {
-    setInput(event.toLowerCase());
-  };
-
-  const params = {
-    order: "id.asc",
-    mineral: `ilike.*${input}*`,
-    id: `gt.0`,
-    limit: PAGE_SIZE,
-  };
-
   return h(ContentPage, [
     h(StickyHeader, { className: "header" }, [
       h(PageBreadcrumbs, {
         title: "Minerals",
       }),
-      h(SearchBar, {
-        placeholder: "Filter by name...",
-        onChange: handleChange,
-      }),
     ]),
-    h(InfiniteScrollView, {
-      params,
+    h(PostgRESTInfiniteScrollView, {
       route: `${apiDomain}/api/pg/minerals`,
-      getNextParams,
-      initialItems: input === "" ? res : [],
-      hasMore,
+      initialItems: res,
       itemComponent: MineralItem,
-      key: input
+      filterable: true,
+      id_key: "id",
+      limit: PAGE_SIZE,
+      searchColumns: ["mineral"],
     })
   ]);
 }
@@ -55,16 +36,4 @@ function MineralItem({ data }) {
     className: "mineral-item",
     title: mineral,
   });
-}
-
-function getNextParams(response, params) {
-  const id = response[response.length - 1]?.id;
-  return {
-    ...params,
-    id: "gt." + id,
-  };
-}
-
-function hasMore(response) {
-  return response.length === PAGE_SIZE; 
 }
