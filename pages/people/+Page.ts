72ade47243e02af762487b810700cd964f8885ea
davidsklar99 <davidsklar99@outlook.com>
2025-08-01 14:43:00 -0400

    Start of mechanism

--- pages/people/+Page.ts
+++ pages/people/+Page.ts
@@ -1,123 +1,36 @@
 import { Image, Navbar, Footer, SearchBar } from "~/components/general";
 import h from "./main.module.sass";
 import { Card, Divider } from "@blueprintjs/core";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ContentPage } from "~/layouts";
+import { fetchPGData } from "~/_utils";
 
 export function Page() {
   const [input, setInput] = useState("");
   const [tags, setTags] = useState([]);
-  const res = [
-    {
-      name: "Shanan Peters",
-      role: "Professor, Database Developer",
-      email: "peters@geology.wisc.edu",
-      link: "http://strata.geology.wisc.edu",
-      image: "shanan.jpg",
-    },
-    {
-      name: "Daven Quinn",
-      role: "Research Scientist, Developer",
-      email: "daven.quinn@wisc.edu",
-      link: "https://davenquinn.com",
-      image: "daven.jpg",
-    },
-    {
-      name: "Evgeny Mazko",
-      role: "Graduate Student",
-      email: "mazko@wisc.edu",
-      link: null,
-      image: "evgeny.jpg",
-    },
-    {
-      name: "Michael McClennen",
-      role: "Senior Programmer Analyst",
-      email: "mmcclenn@geology.wisc.edu",
-      link: "https://geoscience.wisc.edu/geoscience/people/staff/name/michael-mcclennen/",
-      image: "michael.jpg",
-    },
-    {
-      name: "Casey Idzikowski",
-      role: "Research Specialist, Developer (former)",
-      email: null,
-      link: "https://idzikowski-casey.github.io/personal-site/",
-      image: "casey.jpg",
-    },
-    {
-      name: "David Sklar",
-      role: "Undergrad Student",
-      email: "dsklar@wisc.edu",
-      link: null,
-      image: "david.jpg",
-    },
-    {
-      name: "Amy Fromandi",
-      role: null,
-      email: "punkish@eidesis.org",
-      link: null,
-      image: "amy.jpg",
-    },
-    {
-      name: "Daniel Segessenmen",
-      role: "Graduate Student (former)",
-      email: null,
-      link: "http://strata.geology.wisc.edu",
-      image: "daniel.jpg",
-    },
-    {
-      name: "Shan Ye",
-      role: "Graduate Student (former)",
-      email: null,
-      link: "https://www.wisc.edu/directories/person.php?name=Victoria+Khoo&email=vkhoo%40wisc.edu&query=victoria%20khoo",
-      image: "shan.jpg",
-    },
-    {
-      name: "Ben Linzmeier",
-      role: "Postdoctoral Scholar (former)",
-      email: null,
-      link: "http://strata.geology.wisc.edu",
-      image: "ben.jpg",
-    },
-    {
-      name: "Afiqah Rafi",
-      role: "Undergrad Student (former)",
-      email: null,
-      link: "https://www.wisc.edu/directories/person.php?name=Victoria+Khoo&email=vkhoo%40wisc.edu&query=victoria%20khoo",
-      image: "afiqah.jpg",
-    },
-    {
-      name: "Sharon McMullen",
-      role: "Researcher (former)",
-      email: null,
-      link: "http://geoscience.wisc.edu/geoscience/people/student/?id=1007",
-      image: "sharon.jpg",
-    },
-    {
-      name: "Andrew Zaffos",
-      role: "Data Mobilization and Research Scientist",
-      email: "azaffos@email.arizona.edu",
-      link: "http://www.azstrata.org",
-      image: "andrew.jpg",
-    },
-    {
-      name: "Jon Husson",
-      role: "Postdoctoral Researcher (former)",
-      email: "jhusson@uvic.ca",
-      link: "http://www.jonhusson.com",
-      image: "jon.jpg",
-    },
-  ];
 
-  console.log(tags);
+  const [res, setPeople] = useState([]);
+  const [tagList, setTagList] = useState([]);
 
-  const tagList = [
-    "Student",
-    "Researcher",
-    "Developer",
-    "Postdoc",
-    "Research Scientist",
-    "Former",
-  ];
+  useEffect(() => {
+    fetchPGData("/people", {})
+      .then(setPeople)
+      .catch((err) => {
+        console.error("Failed to fetch people:", err);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchPGData("/roles", {})
+      .then(data => setTagList(data.map(role => role.name)))
+      .catch((err) => {
+        console.error("Failed to fetch tags:", err);
+      });
+  }, []);
+
+  if(!res || !tagList) {
+    return h("div.loading", "Loading...");
+  }
 
   const handleInputChange = (e) => {
     const value = e.target.value;
@@ -192,12 +105,12 @@ export function Page() {
   ]);
 }
 
-function PersonCard({ name, role, email, link, image }) {
+function PersonCard({ name, roles, email, website, img_id }) {
   return h("div.person-info", [
-    h(Image, { src: image, className: "back-img" }),
+    h(Image, { src: img_id, className: "back-img" }),
     h("div.description", [
-      h("a.name", { href: link }, name),
-      role ? h("p.role", role) : null,
+      h("a.name", { href: website }, name),
+      roles ? h("p.role", roles.map(role => role.name).join(", ")) : null,
       email ? h("a.email", { href: "mailto: " + email }, email) : null,
     ]),
   ]);
