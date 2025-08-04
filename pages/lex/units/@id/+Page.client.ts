25424df7756fed538901ed96bb6df3dece1e0d3c
davidsklar99 <davidsklar99@outlook.com>
2025-08-04 17:18:50 -0400

    Filtered units page

---
+++ pages/lex/units/@id/+Page.client.ts
@@ -0,0 +1,46 @@
+import { useData } from "vike-react/useData";
+import h from "./main.module.sass";
+import { LexItemPage } from "~/components/lex";
+import { usePageContext } from "vike-react/usePageContext";
+
+export function Page() {
+  const { resData } = useData();
+
+  const id = usePageContext().urlParsed.pathname.split("/")[3];
+
+  const children = [h(MineralDetails, { resData })];
+
+  return LexItemPage({
+    children,
+    id,
+    refs: [],
+    resData,
+    siftLink: "mineral",
+    header: h("div.strat-header", [h("h1.strat-title", resData?.mineral)]),
+  });
+}
+
+function MineralDetails({ resData }) {
+  const {
+    mineral_type,
+    formula,
+    url,
+    hardness_min,
+    hardness_max,
+    crystal_form,
+    mineral_color,
+    lustre,
+  } = resData || {};
+
+  return h("div.mineral-details", [
+    h.if(mineral_type)("p.mineral-type", `Type: ${mineral_type}`),
+    h.if(formula)("p.formula", `Formula: ${formula}`),
+    h.if(hardness_min || hardness_max)("p.hardness", `Hardness: ${hardness_min} - ${hardness_max}`),
+    h.if(crystal_form)("p.crystal-form", `Crystal Form: ${crystal_form}`),
+    h.if(mineral_color)("p.color", `Color: ${mineral_color}`),
+    h.if(lustre)("p.lustre", `Lustre: ${lustre}`),
+    url
+      ? h("a.mineral-url", { href: url, target: "_self" }, "More Info")
+      : null,
+  ]);
+}
