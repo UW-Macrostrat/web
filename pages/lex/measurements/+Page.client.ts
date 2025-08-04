714fae3415f4aed3a34981ce9f3e4cdb7b5f7993
davidsklar99 <davidsklar99@outlook.com>
2025-08-04 12:16:37 -0400

    Use new colors

--- pages/lex/measurements/+Page.client.ts
+++ pages/lex/measurements/+Page.client.ts
@@ -308,11 +308,15 @@ function SelectedMeasurement({ selectedMeasurement, setSelectedMeasurement }) {
         return null;
     }
 
-    const { sample_name, sample_geo_unit, sample_lith, lith_id, age, early_id, late_id, sample_description, ref, type } = data;
+    return h(Measurement, { data, setSelectedMeasurement });
+}
+
+export function Measurement({data, setSelectedMeasurement}) {
+    const { sample_name, sample_geo_unit, sample_lith, lith_id, lith_color, int_name, int_id, int_color, sample_description, ref, type, id } = data;
 
     // Lithology tag component
     let lithProps = {
-        data: { name: sample_lith }
+        data: { name: sample_lith, color: lith_color }
     };
 
     if (lith_id !== 0) {
@@ -321,24 +325,24 @@ function SelectedMeasurement({ selectedMeasurement, setSelectedMeasurement }) {
 
     // Interval tag component
     let ageProps = {
-        data: { name: age }
+        data: { name: int_name, color: int_color }
     };
 
-    if (early_id !== 0) {
-        ageProps.onClick = () => { window.open('/lex/intervals/' + early_id); };
+    if (int_id !== 0) {
+        ageProps.onClick = () => { window.open('/lex/intervals/' + int_id); };
     }
 
     return h("div.selected-measurement", [
-        h(FlexRow, { justifyContent: 'space-between' }, [ 
+        h.if(setSelectedMeasurement)(FlexRow, { justifyContent: 'space-between' }, [ 
             h("h3", "Selected Measurement"),
             h(Icon, { icon: "cross", className: 'close-btn', onClick: () => setSelectedMeasurement(null) }),
         ]),
-        h(Divider),
-        h(DataField, { label: "Name", value: h('a.ref', { href: '/lex/measurements/' + selectedMeasurement, target: "_blank" }, sample_name) }),
+        h.if(setSelectedMeasurement)(Divider),
+        h(DataField, { label: "Name", value: h('a.ref', { href: '/lex/measurements/' + id, target: "_blank" }, sample_name) }),
         h(DataField, { label: "Type", value: type }),
         h(DataField, { label: "Geological Unit", value: sample_geo_unit }),
         h.if(sample_lith)(DataField, { label: "Lithology", value: h(LithologyTag, lithProps) }),
-        h.if(age)(DataField, { label: "Age", value: h(LithologyTag, ageProps) }),
+        h.if(int_id)(DataField, { label: "Age", value: h(LithologyTag, ageProps) }),
         h(DataField, { label: "Description", value: sample_description }),
         h.if(ref.includes("http"))('a.ref', { href: ref, target: "_blank" }, "View Reference"),
     ]);
--- pages/lex/measurements/@id/+Page.client.ts
+++ pages/lex/measurements/@id/+Page.client.ts
@@ -4,13 +4,16 @@ import { LexItemPage } from "~/components/lex";
 import { usePageContext } from "vike-react/usePageContext";
 import { DataField } from "~/components/unit-details";
 import { LithologyTag } from "@macrostrat/data-components";
+import { fetchAPIData } from "~/_utils";
+import { useEffect, useState } from "react";
+import { Measurement } from "../+Page.client";
 
 export function Page() {
   const { resData } = useData();
 
   const id = usePageContext().urlParsed.pathname.split("/")[3];
 
-  const children = [h(SelectedMeasurement, { resData })];
+  const children = [h(Measurement, { data: resData })];
 
   return LexItemPage({
     children,
@@ -23,11 +26,21 @@ export function Page() {
 }
 
 function SelectedMeasurement({ resData }) {
+    const [lithData, setLithData] = useState(null);
+    const [intervalData, setIntervalData] = useState(null);
+
     const { sample_name, sample_geo_unit, sample_lith, lith_id, age, early_id, id, sample_description, ref, type } = resData;
 
     // Lithology tag component
+    useEffect(() => {
+        if (lith_id !== 0) {
+            fetchAPIData("/defs/lithologies", { lith_id })
+                .then(data => setLithData(data[0]))
+        }
+    }, [lith_id]);
+
     let lithProps = {
-        data: { name: sample_lith }
+        data: { name: sample_lith, color: lithData?.color || "#868aa2" }
     };
 
     if (lith_id !== 0) {
@@ -35,12 +48,19 @@ function SelectedMeasurement({ resData }) {
     }
 
     // Interval tag component
+    useEffect(() => {
+        if (age) {
+            fetchAPIData("/defs/intervals", { name_like: age })
+                .then(data => setIntervalData(data[0]))
+        }
+    }, [age]);
+
     let ageProps = {
-        data: { name: age }
+        data: { name: age, color: intervalData?.color || "#868aa2" }
     };
 
-    if (early_id !== 0) {
-        ageProps.onClick = () => { window.open('/lex/intervals/' + early_id); };
+    if (intervalData) {
+        ageProps.onClick = () => { window.open('/lex/intervals/' + intervalData.int_id); };
     }
 
     return h("div.selected-measurement", [
