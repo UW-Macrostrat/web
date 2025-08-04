f2549322d952fb6e4295488fedf79e531eb5be14
Daven Quinn <code@davenquinn.com>
2025-08-04 04:28:29 -0500

    Added a skeletal page

---
+++ pages/dev/column-footprint-editor/+Page.client.ts
@@ -0,0 +1,9 @@
+import h from "@macrostrat/hyper";
+
+export function Page() {
+  return h("div.page", [
+    h("h1", "Column Footprint Editor"),
+    h("p", "This page is under construction."),
+    h("p", "Please check back later for updates."),
+  ]);
+}
