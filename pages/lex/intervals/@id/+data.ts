0e3c26c3fb96b1ae6c5af5245883e22133cdfa92
davidsklar99 <davidsklar99@outlook.com>
2025-09-19 14:43:06 -0500

    Update rest

--- pages/lex/intervals/@id/+data.ts
+++ pages/lex/intervals/@id/+data.ts
@@ -1,5 +1,6 @@
 import { pbdbDomain } from "@macrostrat-web/settings";
 import { fetchAPIData, fetchAPIRefs } from "~/_utils";
+import { getPrevalentTaxa } from "~/components/lex/data-helper";
 
 export async function data(pageContext) {
   const int_id = parseInt(pageContext.urlParsed.pathname.split("/")[3]);
@@ -42,27 +43,7 @@ export async function data(pageContext) {
   const refValues2 = refs2 ? Object.values(refs2) : [];
   const refs = [...refValues1, ...refValues2];
 
-  // Extract column IDs
-  const cols = colData?.features
-    ?.map((feature) => feature.properties.col_id)
-    ?.join(",");
-
-  // Optional: Fetch PBDB taxa data
-  let taxaData = null;
-  if (cols) {
-    try {
-      const response = await fetch(
-        `${pbdbDomain}/data1.2/occs/prevalence.json?limit=5&coll_id=${cols}`
-      );
-      if (response.ok) {
-        taxaData = await response.json();
-      } else {
-        console.warn("PBDB taxa fetch failed with status", response.status);
-      }
-    } catch (err) {
-      console.warn("Error fetching PBDB taxa data:", err);
-    }
-  }
+  const taxaData = await getPrevalentTaxa(fossilsData);
 
   return {
     resData: resData?.[0] ?? null,
--- pages/lex/lithologies/@id/+data.ts
+++ pages/lex/lithologies/@id/+data.ts
@@ -1,5 +1,6 @@
 import { pbdbDomain } from "@macrostrat-web/settings";
 import { fetchAPIData, fetchAPIRefs } from "~/_utils";
+import { getPrevalentTaxa } from "~/components/lex/data-helper";
 
 export async function data(pageContext) {
   const lith_id = parseInt(pageContext.urlParsed.pathname.split("/")[3]);
@@ -43,25 +44,7 @@ export async function data(pageContext) {
   const refValues2 = refs2 ? Object.values(refs2) : [];
   const refs = [...refValues1, ...refValues2];
 
-  // Extract column IDs to fetch taxa data (PBDB)
-  const collectionIds = fossilsData?.features
-    ?.map((feature) => feature.properties.cltn_id)
-
-  let taxaData = null;
-  if (collectionIds && collectionIds.length > 0) {
-    try {
-      const response = await fetch(
-        `${pbdbDomain}/data1.2/occs/prevalence.json?limit=5&coll_id=${collectionIds.join(",")}`
-      );
-      if (response.ok) {
-        taxaData = await response.json();
-      } else {
-        console.warn("PBDB taxa fetch failed with status", response.status);
-      }
-    } catch (err) {
-      console.warn("Error fetching taxa data from PBDB:", err);
-    }
-  }
+  const taxaData = await getPrevalentTaxa(fossilsData);
 
   return {
     resData: resData?.[0] ?? null,
