6deef52e3082b0106195f77ddcd4aa2b0617a68f
Daven Quinn <code@davenquinn.com>
2025-11-29 18:33:40 -0600

    Basic column axis switching'

--- pages/columns/@column/column-inspector/index.ts
+++ pages/columns/@column/column-inspector/index.ts
@@ -1,8 +1,10 @@
 import {
   ColoredUnitComponent,
-  MacrostratDataProvider,
   Column,
+  Identifier,
   MacrostratColumnStateProvider,
+  MacrostratDataProvider,
+  ReferencesField,
 } from "@macrostrat/column-views";
 import { hyperStyled } from "@macrostrat/hyper";
 import { useCallback, useEffect, useMemo, useState } from "react";
@@ -16,6 +18,10 @@ import { ModalUnitPanel } from "./modal-panel";
 import { PageBreadcrumbs } from "~/components";
 import { onDemand } from "~/_utils";
 import { ErrorBoundary } from "@macrostrat/ui-components";
+import { DataField } from "@macrostrat/data-components";
+import { ColumnAxisType } from "@macrostrat/column-components";
+import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
+import { Button, FormGroup, HTMLSelect } from "@blueprintjs/core";
 
 const ColumnMap = onDemand(() => import("./map").then((mod) => mod.ColumnMap));
 
@@ -29,8 +35,55 @@ export function ColumnPage(props) {
   );
 }
 
+const heightAxisTypeAtom = atom<ColumnAxisType>();
+
+function inferHeightAxisType(axisType: ColumnAxisType, units): ColumnAxisType {
+  if (axisType == ColumnAxisType.AGE) {
+    return axisType;
+  }
+  // Infer axis type from units
+  const u0 = units[0];
+  if (u0?.t_pos != null && u0?.b_pos != null) {
+    const t_pos = Number(u0.t_pos);
+    const b_pos = Number(u0.b_pos);
+    const d_pos = t_pos - b_pos;
+    if (d_pos > 0) {
+      return ColumnAxisType.HEIGHT;
+    } else {
+      return ColumnAxisType.DEPTH;
+    }
+  }
+  return null;
+}
+
 function ColumnPageInner({ columnInfo, linkPrefix = "/", projectID }) {
   const { units } = columnInfo;
+
+  const userSetAxisType = useAtomValue(heightAxisTypeAtom);
+  const isSection = columnInfo.col_type == "section";
+
+  console.log(userSetAxisType);
+
+  const defaultAxisType = isSection
+    ? ColumnAxisType.HEIGHT
+    : ColumnAxisType.AGE;
+
+  let axisType = userSetAxisType ?? defaultAxisType;
+  axisType = inferHeightAxisType(axisType, units);
+
+  // Set subsidiary options
+
+  let maxInternalColumns = undefined;
+  let unconformityLabels = true;
+  let showTimescale = true;
+
+  if (isSection) {
+    maxInternalColumns = 1;
+    if (axisType !== ColumnAxisType.AGE) {
+      unconformityLabels = false;
+      showTimescale = false;
+    }
+  }
 
   const [selectedUnitID, setSelectedUnitID] = useState<number>(
     getInitialSelectedUnitID
@@ -45,12 +98,6 @@ function ColumnPageInner({ columnInfo, linkPrefix = "/", projectID }) {
     setHashString(selectedUnitID);
   }, [selectedUnitID]);
 
-  console.log(columnInfo);
-
-  const lon = new Number(columnInfo.lng);
-  const lat = new Number(columnInfo.lat);
-  const zoom = 7;
-
   const onSelectColumn = useCallback(
     (col_id: number) => {
       // do nothing
@@ -62,33 +109,7 @@ function ColumnPageInner({ columnInfo, linkPrefix = "/", projectID }) {
     [setSelectedUnitID]
   );
 
-  let assistantContent = h("div.column-info", [
-    h("h1.page-title", [
-      h("span.col-name", columnInfo.col_name),
-      h.if(columnInfo.col_group != null)("span.subtitle", [
-        h("span.separator", " – "),
-        h(
-          "a",
-          { href: `/columns/groups/${columnInfo.col_group_id}` },
-          h("span.col-group", `${columnInfo.col_group}`)
-        ),
-      ]),
-    ]),
-    h("p.column-details", [
-      h("span.column-id", ["#", columnInfo.col_id]),
-      ", ",
-      h("span.project", ["project ", columnInfo.project_id]),
-      ", ",
-      h(
-        "a",
-        {
-          href: `/map/loc/${lon}/${lat}/column#z=${zoom}&show=columns,geology`,
-        },
-        "show in map"
-      ),
-      ".",
-    ]),
-  ]);
+  let assistantContent = h(ColumnGlobalModal, { data: columnInfo });
 
   if (selectedUnit != null) {
     assistantContent = h(ModalUnitPanel, {
@@ -117,12 +138,15 @@ function ColumnPageInner({ columnInfo, linkPrefix = "/", projectID }) {
               h(Column, {
                 units,
                 unitComponent: ColoredUnitComponent,
-                unconformityLabels: true,
+                unconformityLabels,
                 collapseSmallUnconformities: true,
+                showTimescale,
+                axisType,
                 columnWidth: 300,
                 width: 450,
                 onUnitSelected: setSelectedUnitID,
                 selectedUnit: selectedUnitID,
+                maxInternalColumns,
               }),
             ]),
           ]),
@@ -142,6 +166,75 @@ function ColumnPageInner({ columnInfo, linkPrefix = "/", projectID }) {
       ]),
     ]),
   ]);
+}
+
+function ColumnGlobalModal({ data }) {
+  return h("div.column-assistant", [
+    h(ColumnBasicInfo, { data }),
+    h(ColumnSettingsPanel),
+  ]);
+}
+
+function ColumnBasicInfo({ data, showColumnID = true }) {
+  if (data == null) return null;
+  return h("div.column-info", [
+    h("div.column-title-row", [
+      h("h1", data.col_name),
+      h.if(showColumnID)("h2", h(Identifier, { id: data.col_id })),
+    ]),
+    h(DataField, {
+      row: true,
+      label: "Project",
+      value: data.project_id,
+    }),
+    h(DataField, { row: true, label: "Group", value: data.col_group }),
+    h(ReferencesField, {
+      refs: data.refs,
+      inline: false,
+      row: true,
+      className: "column-refs",
+    }),
+    // Show in Map
+  ]);
+}
+
+let ageAxisOptions = {
+  Height: ColumnAxisType.HEIGHT,
+  Age: ColumnAxisType.AGE,
+};
+
+function ColumnSettingsPanel() {
+  return h("div.column-settings-panel", [h(AxisTypeControl)]);
+}
+
+function AxisTypeControl() {
+  const optionsValues = Object.entries(ageAxisOptions).map(([k, v]) => {
+    return { label: k, value: v };
+  });
+  const [axisType, setAxisType] = useAtom(heightAxisTypeAtom);
+  return h(
+    FormGroup,
+    { label: "Axis type", inline: true },
+    h([
+      h(HTMLSelect, {
+        options: optionsValues,
+        value: axisType,
+        onChange: (evt) => {
+          const value = evt.target.value as ColumnAxisType;
+          // set axis type
+          console.log("Selected axis type:", value);
+          setAxisType(value);
+        },
+      }),
+      h(Button, {
+        minimal: true,
+        small: true,
+        icon: "cross",
+        disabled: axisType == null,
+        onClick: () => setAxisType(null),
+      }),
+    ])
+  );
 }
 
 function getHashParams() {
