10ad3d543bde3e027b02f787381d20be3aa5dd00
Daven Quinn <code@davenquinn.com>
2026-02-15 01:52:13 -0600

    Removed old xdd panel

--- pages/map/map-interface/components/info-drawer/xdd-panel/index.ts
+++ pages/map/map-interface/components/info-drawer/xdd-panel/index.ts
@@ -1,7 +1,5 @@
-import { Spinner } from "@blueprintjs/core";
 import h from "@macrostrat/hyper";
-import Journal from "./Journal";
-import { ExpansionPanel, xDDExpansionPanel } from "@macrostrat/data-components";
+import { xDDExpansionPanel } from "@macrostrat/data-components";
 import { useAppActions, useAppState } from "#/map/map-interface/app-state";
 import { useEffect } from "react";
 
@@ -18,56 +16,4 @@ export function XddExpansionContainer() {
   return h(xDDExpansionPanel, {
     data: xddInfo,
   });
-}
-
-export function XddExpansion2({ xddInfo }) {
-  return h(xDDPanelCore, {
-    className: "regional-panel",
-    data: xddInfo,
-    isFetching: xddInfo == undefined || xddInfo.length === 0,
-  });
-}
-
-export function xDDPanelCore({ isFetching, data: xddInfo, ...rest }) {
-  const groupedData = groupSnippetsByJournal(xddInfo);
-
-  return h(
-    ExpansionPanel,
-    {
-      className: "regional-panel",
-      title: "Primary literature",
-      helpText: "via xDD",
-      ...rest,
-    },
-    [
-      h.if(isFetching)(Spinner),
-      h.if(!isFetching && xddInfo.length > 0)([
-        Array.from(groupedData.entries())?.map(([journal, snippets]) => {
-          return h(Journal, {
-            name: journal,
-            articles: snippets,
-            publisher: snippets[0].publisher,
-            key: journal,
-          });
-        }),
-      ]),
-    ]
-  );
-}
-
-function groupSnippetsByJournal(
-  snippets: XDDSnippet[]
-): Map<string, XDDSnippet[]> {
-  const journals = new Map<string, XDDSnippet[]>();
-  if (!snippets || snippets.length === 0) {
-    return journals;
-  }
-  for (let snippet of snippets) {
-    const { pubname: journal } = snippet;
-    if (!journals.has(journal)) {
-      journals.set(journal, []);
-    }
-    journals.get(journal).push(snippet);
-  }
-  return journals;
 }
