b458997f667dc06005382dd4c5be375b934c82db
Daven Quinn <code@davenquinn.com>
2026-08-02 14:42:01 -0500

    Fixed lithologies

--- packages/lithology-hierarchy/src/index.ts
+++ packages/lithology-hierarchy/src/index.ts
@@ -1,18 +1,7 @@
 import h from "./main.module.sass";
-import { apiV2Prefix } from "@macrostrat-web/settings";
-import { Spinner } from "@blueprintjs/core";
-import { useAPIResult, ErrorCallout } from "@macrostrat/ui-components";
-import { useState } from "react";
-import {
-  nestLiths,
-  nestItems,
-  Lith,
-  nestLithAttributes,
-  LithAttribute,
-} from "./nest-data";
+import { nestItems, Lith, nestLithAttributes } from "./nest-data";
 import { Hierarchy } from "./simple-hierarchy";
 import LexHierarchyInner from "./lex-hierarchy";
-import { LithologyTag } from "~/components";
 import {
   useInteractionProps,
   Tag,
@@ -35,64 +24,6 @@ export function MacrostratHierarchyItem({ data }) {
     color: data.color,
     details,
     ...props,
-  });
-}
-
-export function EnvironmentsHierarchy() {
-  const [error, setError] = useState(null);
-  const res = useAPIResult(
-    `${apiV2Prefix}/defs/environments`,
-    {
-      all: true,
-    },
-    { onError: setError }
-  );
-
-  if (error != null) {
-    return h(ErrorCallout, { error });
-  }
-  if (res == null) {
-    return h(Spinner);
-  }
-  const environments: Lith[] = res.success.data;
-
-  return h(Hierarchy, {
-    data: nestLiths(environments),
-    itemComponent: MacrostratHierarchyItem,
-  });
-}
-
-/** LithAttHierarchyItem */
-function LithAttHierarchyItem({ data }) {
-  const href = "/lex/lith-atts/" + data.lith_att_id;
-  console.log(data);
-  return h(LithologyTag, {
-    data,
-    href,
-  });
-}
-
-export function LithAttsHierarchy() {
-  const [error, setError] = useState(null);
-  const res = useAPIResult(
-    `${apiV2Prefix}/defs/lithology_attributes`,
-    {
-      all: true,
-    },
-    { onError: setError }
-  );
-
-  if (error != null) {
-    return h(ErrorCallout, { error });
-  }
-  if (res == null) {
-    return h(Spinner);
-  }
-  const atts: LithAttribute[] = res.success.data;
-
-  return h(Hierarchy, {
-    data: nestLithAttributes(atts),
-    itemComponent: LithAttHierarchyItem,
   });
 }
 
